4b8660c7868ae2f42995000bfadd83be2cfdc78e
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-04-02 16:49:42 -0400

    Ensure useProjectRedirect is conditional

--- apps/spruce/src/hooks/useProjectRedirect/index.ts
+++ apps/spruce/src/hooks/useProjectRedirect/index.ts
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { useQuery } from "@apollo/client";
 import { useParams, useLocation, useNavigate } from "react-router-dom";
 import { slugs } from "constants/routes";
+import { useToastContext } from "context/toast";
 import { ProjectQuery, ProjectQueryVariables } from "gql/generated/types";
 import { PROJECT } from "gql/queries";
 import { validators } from "utils";
@@ -9,22 +11,27 @@ const { validateObjectId } = validators;
 
 interface UseProjectRedirectProps {
   sendAnalyticsEvent: (projectId: string, projectIdentifier: string) => void;
+  shouldRedirect?: boolean;
 }
 
 /**
  * useProjectRedirect will replace the project id with the project identifier in the URL.
  * @param props - Object containing the following:
  * @param props.sendAnalyticsEvent - analytics event to send upon redirect
+ * @param props.shouldRedirect - boolean to indicate if a redirect should be attempted
  * @returns isRedirecting - boolean to indicate if a redirect is in progress
  */
 export const useProjectRedirect = ({
   sendAnalyticsEvent = () => {},
+  shouldRedirect,
 }: UseProjectRedirectProps) => {
+  const [attemptedRedirect, setAttemptedRedirect] = useState(false);
   const { [slugs.projectIdentifier]: project } = useParams();
+  const dispatchToast = useToastContext();
   const navigate = useNavigate();
   const location = useLocation();
 
-  const needsRedirect = validateObjectId(project);
+  const needsRedirect = validateObjectId(project) && shouldRedirect;
 
   const { loading } = useQuery<ProjectQuery, ProjectQueryVariables>(PROJECT, {
     skip: !needsRedirect,
@@ -36,9 +43,14 @@ export const useProjectRedirect = ({
       const currentUrl = location.pathname.concat(location.search);
       const redirectPathname = currentUrl.replace(project, identifier);
       sendAnalyticsEvent(project, identifier);
-      navigate(redirectPathname);
+      navigate(redirectPathname, { replace: true });
+      setAttemptedRedirect(true);
+    },
+    onError: (e) => {
+      setAttemptedRedirect(true);
+      dispatchToast.error(`There was an error redirecting: ${e.message}`);
     },
   });
 
-  return { isRedirecting: needsRedirect && loading };
+  return { isRedirecting: needsRedirect && loading, attemptedRedirect };
 };
