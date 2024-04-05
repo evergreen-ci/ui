bfcdd72ec3c019ed1897c3fadb7809c7bc4dc671
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-04-05 16:22:04 -0400

    DEVPROD-5469 Update job logs page to support resmoke logs not hosted in logkeeper (#22)

---
+++ apps/spruce/src/pages/jobLogs/Metadata.tsx
@@ -0,0 +1,47 @@
+import { useJobLogsAnalytics } from "analytics";
+import {
+  MetadataCard,
+  MetadataItem,
+  MetadataTitle,
+} from "components/MetadataCard";
+import { StyledLink } from "components/styles";
+import { JobLogsMetadata } from "./types";
+
+export const Metadata: React.FC<{
+  loading: boolean;
+  metadata: JobLogsMetadata;
+}> = ({ loading, metadata }) => {
+  const { sendEvent } = useJobLogsAnalytics(metadata.isLogkeeper);
+
+  return (
+    <MetadataCard loading={loading}>
+      <MetadataTitle>Job log details</MetadataTitle>
+      {metadata.groupID && (
+        <MetadataItem>Group: {metadata.groupID}</MetadataItem>
+      )}
+      {metadata.builder && (
+        <MetadataItem>Builder: {metadata.builder}</MetadataItem>
+      )}
+      {metadata.buildNum && (
+        <MetadataItem>Build number: {metadata.buildNum}</MetadataItem>
+      )}
+      {metadata.allLogsURL && (
+        <MetadataItem>
+          <StyledLink
+            data-cy="complete-test-logs-link"
+            href={metadata.allLogsURL}
+            target="_blank"
+            onClick={() => {
+              sendEvent({
+                name: "Clicked complete logs link",
+                buildId: metadata.buildId,
+              });
+            }}
+          >
+            Complete logs for all tests
+          </StyledLink>
+        </MetadataItem>
+      )}
+    </MetadataCard>
+  );
+};
---
+++ apps/spruce/src/pages/jobLogs/index.tsx
@@ -0,0 +1,99 @@
+import styled from "@emotion/styled";
+import Button from "@leafygreen-ui/button";
+import { H3 } from "@leafygreen-ui/typography";
+import { useParams } from "react-router-dom";
+import PageTitle from "components/PageTitle";
+import {
+  PageContent,
+  PageLayout,
+  PageSider,
+  PageWrapper,
+} from "components/styles";
+import TaskStatusBadge from "components/TaskStatusBadge";
+import { getTaskRoute, slugs } from "constants/routes";
+import { size } from "constants/tokens";
+import { useToastContext } from "context/toast";
+import { JobLogsTable } from "./JobLogsTable";
+import { Metadata } from "./Metadata";
+import useJobLogsPageData from "./useJobLogs";
+
+interface JobLogsProps {
+  isLogkeeper: boolean;
+}
+const JobLogs: React.FC<JobLogsProps> = ({ isLogkeeper }) => {
+  const {
+    [slugs.buildId]: buildIdFromParams,
+    [slugs.taskId]: taskIdFromParams,
+    [slugs.execution]: executionFromParams,
+    [slugs.groupId]: groupIdFromParams,
+  } = useParams();
+
+  const dispatchToast = useToastContext();
+
+  const { loading, metadata, resultsToRender, title } = useJobLogsPageData({
+    buildId: buildIdFromParams,
+    execution: executionFromParams,
+    groupId: groupIdFromParams,
+    onError: (err) => {
+      dispatchToast.error(err);
+    },
+    taskId: taskIdFromParams,
+    isLogkeeper,
+  });
+
+  return (
+    <PageWrapper>
+      <PageTitle
+        pageTitle={`Job Logs - ${title}`}
+        title="Job Logs"
+        loading={loading}
+        size="large"
+        badge={null}
+        subtitle={
+          <SubtitleContainer>
+            <H3>{metadata.displayName}</H3>{" "}
+            <TaskStatusBadge status={metadata.taskStatus} />
+          </SubtitleContainer>
+        }
+        buttons={
+          <Button
+            href={getTaskRoute(metadata.taskId, {
+              execution: metadata.execution,
+            })}
+            data-cy="task-link"
+          >
+            Task page
+          </Button>
+        }
+      />
+
+      <StyledPageLayout hasSider>
+        <PageSider>
+          <Metadata metadata={metadata} loading={loading} />
+        </PageSider>
+        <PageLayout>
+          <PageContent>
+            <JobLogsTable
+              buildId={buildIdFromParams}
+              tests={resultsToRender}
+              isLogkeeper={isLogkeeper}
+              loading={loading}
+            />
+          </PageContent>
+        </PageLayout>
+      </StyledPageLayout>
+    </PageWrapper>
+  );
+};
+
+const StyledPageLayout = styled(PageLayout)`
+  padding-top: ${size.m};
+`;
+
+const SubtitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  gap: ${size.s};
+`;
+export default JobLogs;
