5b939308ec7364858653e6b070933398efe12326
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-04-17 13:06:24 -0400

    Add extra styling

--- apps/spruce/src/components/SettingsCard.tsx
+++ apps/spruce/src/components/SettingsCard.tsx
@@ -1,10 +1,20 @@
 import styled from "@emotion/styled";
 import Card from "@leafygreen-ui/card";
+import { palette } from "@leafygreen-ui/palette";
 import { H3, H3Props } from "@leafygreen-ui/typography";
 import { size } from "constants/tokens";
 
+const { gray } = palette;
+
 export const SettingsCardTitle = styled(H3)<H3Props>`
   margin: ${size.m} 0 ${size.s} 0;
+  :hover {
+    ::after {
+      content: "#";
+      margin-left: ${size.xs};
+      color: ${gray.dark2};
+    }
+  }
 `;
 
 export const formComponentSpacingCSS = "margin-bottom: 48px;";
