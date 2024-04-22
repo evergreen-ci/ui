92ee5be68d14d18669bc3fbfb7ef5804bc72b166
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-04-22 11:24:17 -0400

    Add some unit tests

--- apps/spruce/src/hooks/useScrollToAnchor/index.ts
+++ apps/spruce/src/hooks/useScrollToAnchor/index.ts
@@ -1,14 +1,17 @@
 import { useEffect, useRef } from "react";
+import { useLocation } from "react-router-dom";
 
 /**
  * `useScrollToAnchor` scrolls to an anchor element on the page if the URL contains an anchor.
  */
 const useScrollToAnchor = () => {
-  // Get anchor from the URL
-  const anchor = window.location.hash.slice(1);
+  const { hash } = useLocation();
+  const anchor = hash.slice(1);
+
   //   Delay the scroll until the next tick to ensure the anchor element is rendered
   const timeout = useRef<NodeJS.Timeout>(null);
   useEffect(() => {
+    if (anchor === "") return;
     timeout.current = setTimeout(() => {
       const element = document.getElementById(anchor);
       if (element) {
@@ -16,6 +19,7 @@ const useScrollToAnchor = () => {
       }
     }, 500);
   }, [anchor]);
+
   useEffect(
     () => () => {
       clearTimeout(timeout.current);
