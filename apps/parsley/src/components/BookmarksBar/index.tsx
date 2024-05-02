4af7cc6310c73ca4e0d770e3e65c7f2bf5528762
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-05-02 11:28:21 -0400

    Add confirmation dialog when clearing all bookmarks in Parsley

--- apps/parsley/src/components/BookmarksBar/index.tsx
+++ apps/parsley/src/components/BookmarksBar/index.tsx
@@ -1,10 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import styled from "@emotion/styled";
 import Button from "@leafygreen-ui/button";
 import { palette } from "@leafygreen-ui/palette";
 import Tooltip from "@leafygreen-ui/tooltip";
 import { useLogWindowAnalytics } from "analytics";
 import Icon from "components/Icon";
+import Popconfirm from "components/Popconfirm";
 import { QueryParams } from "constants/queryParams";
 import { size, zIndex } from "constants/tokens";
 import { useQueryParam } from "hooks/useQueryParam";
@@ -27,6 +28,9 @@ const BookmarksBar: React.FC<BookmarksBarProps> = ({
 }) => {
   const { sendEvent } = useLogWindowAnalytics();
 
+  const clearButtonRef = useRef(null);
+  const [clearButtonConfirmationOpen, setClearButtonConfirmationOpen] =
+    useState(false);
   const [shareLine] = useQueryParam<number | undefined>(
     QueryParams.ShareLine,
     undefined,
@@ -61,15 +65,24 @@ const BookmarksBar: React.FC<BookmarksBarProps> = ({
 
   return (
     <Container>
+      <Popconfirm
+        onConfirm={() => {
+          setBookmarks([]);
+          sendEvent({ name: "Cleared All Bookmarks" });
+        }}
+        open={clearButtonConfirmationOpen}
+        refEl={clearButtonRef}
+        setOpen={setClearButtonConfirmationOpen}
+      >
+        <div>Are you sure you want to clear all bookmarks?</div>
+      </Popconfirm>
       <Tooltip
         popoverZIndex={zIndex.tooltip}
         trigger={
           <StyledButton
+            ref={clearButtonRef}
             data-cy="clear-bookmarks"
-            onClick={() => {
-              setBookmarks([]);
-              sendEvent({ name: "Cleared All Bookmarks" });
-            }}
+            onClick={() => setClearButtonConfirmationOpen(true)}
             size="xsmall"
           >
             Clear
