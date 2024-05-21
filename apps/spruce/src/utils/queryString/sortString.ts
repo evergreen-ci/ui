412e8f753cc51d7bf0dda8acd50dd63dcd8df3da
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-05-21 14:18:17 -0400

    Refactor sort string parsing to be more consistent

--- apps/spruce/src/utils/queryString/sortString.ts
+++ apps/spruce/src/utils/queryString/sortString.ts
@@ -1,72 +1,54 @@
-import { Key, SorterResult } from "antd/es/table/interface";
-import { Task, SortDirection, TaskSortCategory } from "gql/generated/types";
+import { SortDirection } from "gql/generated/types";
 
-export const getSortString = (columnKey: Key, direction: SortDirection) =>
-  columnKey && direction ? `${columnKey}:${direction}` : undefined;
+type WithDirection<K extends string> = {
+  direction: SortDirection;
+} & Record<K, any>;
 
-const shortenSortOrder = (order: string) =>
-  order === "ascend" ? SortDirection.Asc : SortDirection.Desc;
+export const getSortString = (columnKey: string, direction: SortDirection) =>
+  columnKey && direction ? `${columnKey}:${direction}` : "";
 
-// takes sort input from the antd table and translates into part of the query string
-// if sort field is being unset, returns undefined
-export const toSortString = (
-  sorts: SorterResult<Task> | SorterResult<Task>[],
-) => {
-  let sortStrings: string[] = [];
-  if (Array.isArray(sorts)) {
-    sortStrings = sorts.map(({ columnKey, order }) =>
-      order ? getSortString(columnKey, shortenSortOrder(order)) : undefined,
-    );
-  } else {
-    sortStrings = [
-      sorts.order
-        ? getSortString(sorts.columnKey, shortenSortOrder(sorts.order))
-        : undefined,
-    ];
-  }
+/**
+ * Takes sort input from a table and translates it into part of the query string.
+ * If the sort field is being unset, returns undefined.
+ * @param arr - The array of sort fields to use.
+ * @param key - The key to use for the sort field.
+ * @returns The translated part of the query string, or undefined if the sort field is being unset.
+ */
+export const toSortString = <K extends string>(
+  arr: WithDirection<K>[],
+  key: K,
+): string => {
+  const sortStrings: string[] = arr.map(({ direction, [key]: sortKey }) =>
+    getSortString(sortKey, direction),
+  );
 
-  return sortStrings.some((s) => s)
-    ? sortStrings.filter(Boolean).join(";")
-    : undefined;
+  return sortStrings.filter(Boolean).join(";");
 };
 
-// takes a sort query string and parses it into valid GQL params
-// By default, uses keys for task's SortOrder type, but sort field keys can be passed in for use with e.g. tests' TestSortOptions
-export const parseSortString = <
-  T extends Record<string, SortDirection | TaskSortCategory>,
->(
-  sortQuery: string | string[],
-  options: {
-    sortByKey: keyof T;
-    sortDirKey: keyof T;
-  } = { sortByKey: "Key", sortDirKey: "Direction" },
-): T[] => {
-  let sorts: T[] = [];
-  let sortArray: string[] = [];
-  if (typeof sortQuery === "string") {
-    sortArray = sortQuery.split(";");
-  } else {
-    sortArray = sortQuery;
-  }
-  if (sortArray?.length > 0) {
-    sortArray.forEach((singleSort) => {
-      const parts = singleSort.split(":");
-      if (parts.length !== 2) {
-        return;
-      }
-      if (
-        !Object.values(TaskSortCategory).includes(parts[0] as TaskSortCategory)
-      ) {
-        return;
-      }
-      if (!Object.values(SortDirection).includes(parts[1] as SortDirection)) {
-        return;
-      }
-      sorts = sorts.concat({
-        [options.sortByKey]: parts[0] as TaskSortCategory,
-        [options.sortDirKey]: parts[1] as SortDirection,
-      } as T);
-    });
-  }
-  return sorts;
+/**
+ * Takes a sort query string and parses it into valid GQL params.
+ * By default, uses keys for task's SortOrder type, but sort field keys can be passed in for use with e.g. tests' TestSortOptions.
+ * @param str The sort query string.
+ * @param key The key to use for the sort field.
+ * @returns The parsed sort query string.
+ */
+export const parseSortString = <K extends string>(
+  str: string,
+  key: K,
+): WithDirection<K>[] => {
+  const arr = str.split(";").map((item) => {
+    const [sortKey, direction] = item.split(":");
+    if (!sortKey || !direction) {
+      return undefined;
+    }
+    if (!Object.values(SortDirection).includes(direction as SortDirection)) {
+      return undefined;
+    }
+    return {
+      direction,
+      [key]: sortKey,
+    } as WithDirection<K>;
+  });
+
+  return arr.filter(Boolean);
 };
