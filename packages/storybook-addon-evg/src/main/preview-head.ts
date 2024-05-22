6d08e864e7faa024cac39346065aa8dde1b3350d
Sophie Stadler <sophie.stadler@mongodb.com>
2024-05-22 14:15:02 -0400

    Upgrade Storybook

--- packages/storybook-addon-evg/src/main/preview-head.ts
+++ packages/storybook-addon-evg/src/main/preview-head.ts
@@ -1,6 +1,6 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
-export const previewHead: StorybookConfig["previewHead"] = (head: string) => `
+export const previewHead: StorybookConfig["previewHead"] = (head) => `
 ${head}
 <link rel="preconnect" href="https://fonts.gstatic.com" />
 <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,400;0,600;1,400&display=swap" rel="stylesheet" />
