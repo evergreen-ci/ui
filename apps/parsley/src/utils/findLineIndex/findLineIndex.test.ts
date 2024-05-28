c57950d9ea4e67150be668a3ed0176d484a84775
Arjun Patel <arjun453@gmail.com>
2024-05-28 14:05:46 -0400

    update copy

--- apps/parsley/src/utils/findLineIndex/findLineIndex.test.ts
+++ apps/parsley/src/utils/findLineIndex/findLineIndex.test.ts
@@ -12,18 +12,18 @@ const processedLines: ProcessedLogLines = [
 ];
 
 describe("findLineIndex", () => {
-  it("should correctly determine index when line number exists in array", () => {
+  it("should correctly determine index when line number exists directly in the array and is not represented in a Range object", () => {
     expect(findLineIndex(processedLines, 0)).toBe(0);
     expect(findLineIndex(processedLines, 3)).toBe(2);
     expect(findLineIndex(processedLines, 6)).toBe(4);
   });
 
-  it("should correctly determine index when line number exists in nested array", () => {
+  it("should correctly determine index when line number is represented in a Range object", () => {
     expect(findLineIndex(processedLines, 1)).toBe(1);
     expect(findLineIndex(processedLines, 4)).toBe(3);
   });
 
-  it("should return -1 when line number does not exist in array", () => {
+  it("should return -1 when line number is not represented in the array", () => {
     expect(findLineIndex(processedLines, -1)).toBe(-1);
     expect(findLineIndex(processedLines, 11)).toBe(-1);
   });
