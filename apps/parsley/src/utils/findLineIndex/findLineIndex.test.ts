9dd3effb423ee4c5666121cc3bf0ad55013f881a
SupaJoon <arjun453@gmail.com>
2024-05-30 13:56:27 -0400

    DEVPROD-5290, DEVPROD-5299: Represent SkippedLines with a range instead of a list (#134)

--- apps/parsley/src/utils/findLineIndex/findLineIndex.test.ts
+++ apps/parsley/src/utils/findLineIndex/findLineIndex.test.ts
@@ -1,20 +1,29 @@
+import { ProcessedLogLines } from "types/logs";
 import { findLineIndex } from ".";
 
-const processedLines = [0, [1, 2], 3, [4, 5], 6, [7, 8, 9], 10];
+const processedLines: ProcessedLogLines = [
+  0,
+  { range: { end: 3, start: 1 }, rowType: "SkippedLines" },
+  3,
+  { range: { end: 6, start: 4 }, rowType: "SkippedLines" },
+  6,
+  { range: { end: 10, start: 7 }, rowType: "SkippedLines" },
+  10,
+];
 
 describe("findLineIndex", () => {
-  it("should correctly determine index when line number exists in array", () => {
+  it("should correctly determine index when line number exists directly in the array and is not represented in a Range object", () => {
     expect(findLineIndex(processedLines, 0)).toBe(0);
     expect(findLineIndex(processedLines, 3)).toBe(2);
     expect(findLineIndex(processedLines, 6)).toBe(4);
   });
 
-  it("should correctly determine index when line number exists in nested array", () => {
+  it("should correctly determine index when line number is represented in a Range object", () => {
     expect(findLineIndex(processedLines, 1)).toBe(1);
     expect(findLineIndex(processedLines, 4)).toBe(3);
   });
 
-  it("should return -1 when line number does not exist in array", () => {
+  it("should return -1 when line number is not represented in the array", () => {
     expect(findLineIndex(processedLines, -1)).toBe(-1);
     expect(findLineIndex(processedLines, 11)).toBe(-1);
   });
