d62c415d64ec8d53c05dbcc8923798fa725bd63c
Arjun Patel <arjun453@gmail.com>
2024-06-03 13:39:50 -0400

    toast and report error

--- apps/parsley/src/hooks/useSections/index.ts
+++ apps/parsley/src/hooks/useSections/index.ts
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { useToastContext } from "context/toast";
+import { reportError } from "utils/errorReporting";
 import { SectionEntry, parseSections } from "./utils";
 
 export interface Result {
@@ -9,14 +11,20 @@ interface Props {
   sectionsEnabled?: boolean;
 }
 export const useSections = ({ logs, sectionsEnabled }: Props): Result => {
+  const dispatchToast = useToastContext();
   const [sectionData, setSectionData] = useState<SectionEntry[] | undefined>();
 
   const shouldParse =
     logs.length && sectionsEnabled && sectionData === undefined;
-
   useEffect(() => {
     if (shouldParse) {
-      const parseResult = parseSections(logs);
+      let parseResult;
+      try {
+        parseResult = parseSections(logs);
+      } catch (e) {
+        dispatchToast.error("An error occurred while parsing log sections.");
+        reportError(e as Error).severe();
+      }
       setSectionData(parseResult);
     }
   }, [shouldParse]);
