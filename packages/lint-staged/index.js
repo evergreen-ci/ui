6e900a1a9694087daf41645e766f75076c65680e
Sophie Stadler <sophie.stadler@mongodb.com>
2024-06-04 14:45:20 -0400

    Update lint-staged command

--- packages/lint-staged/index.js
+++ packages/lint-staged/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-  "**/*.{js,ts,tsx}": ["yarn eslint:staged", "yarn prettier"],
+  "**/*.{js,ts,tsx,graphql,gql}": ["yarn eslint:staged", "yarn prettier"],
   "*.{ts,tsx}": () => ["yarn check-types"], // For TypeScript files, run tsc, and gql schema check
 };
