9d3c76216d1613fe2547d7a4d29e410d8fc481a0
Arjun Patel <arjun453@gmail.com>
2024-06-05 12:10:53 -0400

    update comments

--- apps/parsley/src/hooks/useSections/utils.ts
+++ apps/parsley/src/hooks/useSections/utils.ts
@@ -1,12 +1,17 @@
 import { Range } from "types/logs";
 import { trimSeverity } from "utils/string";
 
+enum SectionStatus {
+  Running = "Running",
+  Finished = "Finished",
+}
+
 interface SectionLineMetadata {
   functionName: string;
-  status: "Running" | "Finished";
+  status: SectionStatus;
 }
 /**
- * Parses a log line to extract metadata about a section entry.
+ * `processLine` parses a log line to extract metadata about a section entry.
  * @param str - The log line to be processed.
  * @returns The metadata about the section entry
  * or null if the input line does not indicate the start or end of a section.
@@ -15,12 +20,9 @@ export const processLine = (str: string): SectionLineMetadata | null => {
   const regex = /(Running|Finished) command '[^']+' in function '([^']+)'.*/;
   const match = trimSeverity(str).match(regex);
   if (match) {
-    const status = match[1] as "Running" | "Finished";
-    const functionName = match[2];
-
     return {
-      functionName,
-      status,
+      functionName: match[2],
+      status: match[1] as SectionStatus,
     };
   }
   return null;
@@ -32,7 +34,7 @@ interface SectionEntry {
 }
 
 /**
- * Reduces the logs array to accumulate section data. Each entry in the returned array represents a section.
+ * `reduceFn` reduces the logs array to accumulate section data. Each entry in the returned array represents a section.
  * @param accum - The accumulated section data.
  * @param line - The current line being processed.
  * @param logIndex - The log index of the current line.
@@ -49,7 +51,7 @@ const reduceFn = (
     return accum;
   }
   const sections = [...accum];
-  if (currentLine.status === "Finished") {
+  if (currentLine.status === SectionStatus.Finished) {
     if (sections.length === 0) {
       throw new Error(
         "Log file is showing a finished section without a running section before it. This should not happen.",
@@ -59,7 +61,7 @@ const reduceFn = (
     sections[sections.length - 1].range.end = logIndex + 1;
     // If the current line indicates a running section and either no sections in the accumulator or the previous section is different from the current one, add a new section
   } else if (
-    currentLine.status === "Running" &&
+    currentLine.status === SectionStatus.Running &&
     (sections.length === 0 ||
       sections[sections.length - 1].functionName !== currentLine.functionName)
   ) {
@@ -78,7 +80,7 @@ const reduceFn = (
 };
 
 /**
- * Parses the raw logs array to extract section data.
+ * `parseSections` parses the raw logs array to extract section data.
  * @param logs - The array of log lines to be parsed.
  * @returns An array of section entries representing the parsed sections.
  */
