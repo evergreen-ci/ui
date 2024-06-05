c90d6e3bb89369180307e96aa90acc7f48633688
Sophie Stadler <sophie.stadler@mongodb.com>
2024-06-05 16:16:22 -0400

    DEVPROD-4200: Add temporary exemption selector (#153)

--- apps/spruce/src/components/Spawn/utils/hostUptime.ts
+++ apps/spruce/src/components/Spawn/utils/hostUptime.ts
@@ -1,5 +1,12 @@
 import { arraySymmetricDifference } from "@evg-ui/lib/utils/array";
-import { differenceInHours, isTomorrow, parse } from "date-fns";
+import { setToUTCMidnight } from "@leafygreen-ui/date-utils";
+import {
+  isAfter,
+  isBefore,
+  differenceInHours,
+  isTomorrow,
+  parse,
+} from "date-fns";
 import { ValidateProps } from "components/SpruceForm";
 import { days } from "constants/fieldMaps";
 import { SleepSchedule, SleepScheduleInput } from "gql/generated/types";
@@ -32,6 +39,7 @@ export type HostUptime = {
     };
   };
   details?: null;
+  temporarilyExemptUntil?: string;
 };
 
 type ValidateInput = {
@@ -104,12 +112,19 @@ export const getEnabledHoursCount = (
 };
 
 export const getSleepSchedule = (
-  { sleepSchedule, useDefaultUptimeSchedule }: HostUptime,
+  {
+    sleepSchedule,
+    temporarilyExemptUntil,
+    useDefaultUptimeSchedule,
+  }: HostUptime,
   timeZone: string,
 ): SleepScheduleInput => {
   if (useDefaultUptimeSchedule) {
     return {
       ...defaultSleepSchedule,
+      ...(temporarilyExemptUntil
+        ? { temporarilyExemptUntil: new Date(temporarilyExemptUntil) }
+        : {}),
       timeZone,
     };
   }
@@ -127,6 +142,9 @@ export const getSleepSchedule = (
     permanentlyExempt: false,
     timeZone,
     shouldKeepOff: false,
+    ...(temporarilyExemptUntil
+      ? { temporarilyExemptUntil: new Date(temporarilyExemptUntil) }
+      : {}),
     // @ts-expect-error: FIXME. This comment was added by an automated script.
     wholeWeekdaysOff: enabledWeekdays.reduce((accum, isEnabled, i) => {
       if (!isEnabled) {
@@ -159,10 +177,16 @@ export const defaultSleepSchedule: Optional<SleepScheduleInput, "timeZone"> = {
 export const getHostUptimeFromGql = (
   sleepSchedule: Optional<SleepSchedule, "timeZone">,
 ): HostUptime => {
-  const { dailyStartTime, dailyStopTime, wholeWeekdaysOff } = sleepSchedule;
+  const {
+    dailyStartTime,
+    dailyStopTime,
+    temporarilyExemptUntil,
+    wholeWeekdaysOff,
+  } = sleepSchedule;
 
   return {
     useDefaultUptimeSchedule: matchesDefaultUptimeSchedule(sleepSchedule),
+    temporarilyExemptUntil: temporarilyExemptUntil?.toString() ?? "",
     sleepSchedule: {
       enabledWeekdays: new Array(7)
         .fill(false)
@@ -218,9 +242,26 @@ export const validator = (({ expirationDetails }, errors) => {
   const { hostUptime, noExpiration } = expirationDetails ?? {};
   if (!hostUptime || noExpiration === false) return errors;
 
-  const { sleepSchedule, useDefaultUptimeSchedule } = hostUptime;
-  // @ts-expect-error: FIXME. This comment was added by an automated script.
-  const { timeSelection } = sleepSchedule;
+  const { sleepSchedule, temporarilyExemptUntil, useDefaultUptimeSchedule } =
+    hostUptime;
+
+  if (temporarilyExemptUntil) {
+    // LG Date Picker widget provides visual validation but doesn't provide a way to access its error state. Replicate its min/max validation here.
+    const selectedDate = new Date(temporarilyExemptUntil);
+    if (
+      !(
+        isAfter(exemptionRange.disableAfter, selectedDate) &&
+        isBefore(exemptionRange.disableBefore, selectedDate)
+      )
+    ) {
+      // @ts-expect-error
+      errors.expirationDetails?.hostUptime?.temporarilyExemptUntil?.addError?.(
+        "Invalid date selected; sleep can only be disabled for up to one month.",
+      );
+    }
+  }
+
+  const { timeSelection } = sleepSchedule ?? {};
 
   if (useDefaultUptimeSchedule) {
     return errors;
@@ -304,4 +345,12 @@ export const getNextHostStart = (
     ? "tomorrow"
     : days[nextStartDate.getDay()];
   return { nextStartDay, nextStartTime: null };
+};
+
+const today = new Date(Date.now());
+export const exemptionRange = {
+  disableBefore: setToUTCMidnight(today),
+  disableAfter: setToUTCMidnight(
+    new Date(today.setMonth(today.getMonth() + 1)),
+  ),
 };
