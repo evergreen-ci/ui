f00415ef4a76aa8ffe734a54effff2720cabf069
Arjun Patel <arjun453@gmail.com>
2024-06-07 14:14:42 -0400

    Update binary search to SectionHeaders

--- apps/parsley/src/utils/findLineIndex/findLineIndex.test.ts
+++ apps/parsley/src/utils/findLineIndex/findLineIndex.test.ts
@@ -9,6 +9,29 @@ const processedLines: ProcessedLogLines = [
   6,
   { range: { end: 10, start: 7 }, rowType: "SkippedLines" },
   10,
+  {
+    functionName: "f-1",
+    isOpen: true,
+    range: { end: 13, start: 10 },
+    rowType: "SectionHeader",
+  },
+  11,
+  12,
+  {
+    functionName: "f-2",
+    isOpen: false,
+    range: { end: 15, start: 13 },
+    rowType: "SectionHeader",
+  },
+  {
+    functionName: "f-3",
+    isOpen: true,
+    range: { end: 17, start: 15 },
+    rowType: "SectionHeader",
+  },
+  15,
+  16,
+  17,
 ];
 
 describe("findLineIndex", () => {
@@ -18,13 +41,20 @@ describe("findLineIndex", () => {
     expect(findLineIndex(processedLines, 6)).toBe(4);
   });
 
-  it("should correctly determine index when line number is represented in a Range object", () => {
+  it("should correctly determine index when line number is represented in a Range object belonging to a SkippedLinesRow", () => {
     expect(findLineIndex(processedLines, 1)).toBe(1);
     expect(findLineIndex(processedLines, 4)).toBe(3);
   });
 
   it("should return -1 when line number is not represented in the array", () => {
     expect(findLineIndex(processedLines, -1)).toBe(-1);
-    expect(findLineIndex(processedLines, 11)).toBe(-1);
+    expect(findLineIndex(processedLines, 18)).toBe(-1);
+  });
+
+  it("should correctly determine index when line number is represented in a Range object belonging to a closed SectionHeaderRow", () => {
+    expect(findLineIndex(processedLines, 14)).toBe(10);
+  });
+  it("should correctly determine index when line number is represented in a Range object belonging to an open SectionHeaderRow", () => {
+    expect(findLineIndex(processedLines, 16)).toBe(13);
   });
 });
