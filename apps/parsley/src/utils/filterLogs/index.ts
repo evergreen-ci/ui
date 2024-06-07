ea97e722ba83fd3fb82e9485c8a90b95e61347c0
Arjun Patel <arjun453@gmail.com>
2024-06-07 13:42:35 -0400

    Update filter logs to create sections in ProcessedLogLines

--- apps/parsley/src/utils/filterLogs/index.ts
+++ apps/parsley/src/utils/filterLogs/index.ts
@@ -1,3 +1,4 @@
+import { SectionEntry } from "hooks/useSections/utils";
 import { ExpandedLines, ProcessedLogLines } from "types/logs";
 import { isExpanded } from "utils/expandedLines";
 import { newSkippedLinesRow } from "utils/logRow";
@@ -11,18 +12,22 @@ type FilterLogsParams = {
   expandedLines: ExpandedLines;
   expandableRows: boolean;
   failingLine: number | undefined;
+  sectionData: SectionEntry[] | undefined;
+  sectionsEnabled: boolean;
 };
 
 /**
  * `filterLogs` processes log lines according to what filters, bookmarks, share line, and expanded lines are applied.
  * @param options - an object containing the parameters
+ * @param options.bookmarks - list of line numbers representing bookmarks
+ * @param options.expandableRows - specifies if expandable rows is enabled
+ * @param options.expandedLines - an array of intervals representing expanded ranges
+ * @param options.failingLine - a line number representing the failing line
  * @param options.logLines - list of strings representing the log lines
  * @param options.matchingLines - set of numbers representing which lines match the applied filters
- * @param options.bookmarks - list of line numbers representing bookmarks
+ * @param options.sectionData - an array of objects representing the sections
+ * @param options.sectionsEnabled - specifies if sections are enabled
  * @param options.shareLine - a line number representing a share line
- * @param options.expandedLines - an array of intervals representing expanded ranges
- * @param options.expandableRows - specifies if expandable rows is enabled
- * @param options.failingLine - a line number representing the failing line
  * @returns an array of numbers that indicates which log lines should be displayed, and which log lines
  * should be collapsed
  */
@@ -34,11 +39,32 @@ const filterLogs = (options: FilterLogsParams): ProcessedLogLines => {
     failingLine,
     logLines,
     matchingLines,
+    sectionData,
+    sectionsEnabled,
     shareLine,
   } = options;
-  // If there are no filters or expandable rows is not enabled, then we don't have to do any
-  // processing.
+  // If there are no filters or expandable rows is not enabled, then we only have to process sections if they exist and are enabled.
   if (matchingLines === undefined) {
+    if (sectionsEnabled && sectionData?.length) {
+      let sectionIndex = 0;
+      const filteredLines: ProcessedLogLines = [];
+      logLines.reduce((arr, _logLine, idx) => {
+        const section = sectionData[sectionIndex];
+        const isSectionStart = section && idx === section.range.start;
+        if (isSectionStart) {
+          arr.push({
+            functionName: section.functionName,
+            isOpen: true,
+            range: section.range,
+            rowType: "SectionHeader",
+          });
+          sectionIndex += 1;
+        }
+        arr.push(idx);
+        return arr;
+      }, filteredLines);
+      return filteredLines;
+    }
     return logLines.map((_, idx) => idx);
   }
 
