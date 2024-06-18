b5be7ba080dc856b2083ccecf68d6c9e695cbf62
Arjun Patel <arjun453@gmail.com>
2024-06-18 11:44:13 -0400

    Revert "intialize sentry behind auth for spuce"
    This reverts commit 0540e65a196ca584404c79496aaecdae65df7fb4.

--- apps/spruce/src/components/Content/Layout.tsx
+++ apps/spruce/src/components/Content/Layout.tsx
@@ -18,7 +18,6 @@ import { UserQuery, UserQueryVariables } from "gql/generated/types";
 import { USER } from "gql/queries";
 import { useUserSettings } from "hooks";
 import { useAnnouncementToast } from "hooks/useAnnouncementToast";
-import { useInitializeErrorHandling } from "hooks/useInitializeErrorHandling";
 import { isProduction } from "utils/environmentVariables";
 
 const { gray, white } = palette;
@@ -27,7 +26,6 @@ const shouldDisableForTest =
   !isProduction() && Cookies.get(CY_DISABLE_NEW_USER_WELCOME_MODAL) === "true";
 
 export const Layout: React.FC = () => {
-  useInitializeErrorHandling();
   const { isAuthenticated } = useAuthStateContext();
   useAnalyticsAttributes();
   useAnnouncementToast();
