d499720a229e4202ef29c33c2cd5241f3830a54b
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-06-21 18:37:08 -0400

    DEVPROD-8345 Remove tab active/not active events (#196)

--- apps/spruce/src/analytics/activity/useActivityAnalytics.ts
+++
@@ -1,7 +0,0 @@
-import { useAnalyticsRoot } from "analytics/useAnalyticsRoot";
-
-type Action =
-  | { name: "Tab Active"; status: "online" | "visible" }
-  | { name: "Tab Not Active" };
-
-export const useActivityAnalytics = () => useAnalyticsRoot<Action>("Polling");
