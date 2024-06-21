7e11b068455f0c5f037a7891926c7fa52fe9479b
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-06-21 16:52:55 -0400

    Remove tab active/not active events

--- apps/spruce/src/components/Header/index.tsx
+++ apps/spruce/src/components/Header/index.tsx
@@ -11,8 +11,8 @@ import { Navbar } from "./Navbar";
 // Since the Header is present on all Spruce pages, we can monitor network status and page visibility
 // for the entire app here.
 export const Header: React.FC = () => {
-  usePageVisibility(true);
-  useNetworkStatus(true);
+  usePageVisibility();
+  useNetworkStatus();
   return (
     <StyledHeader>
       <Navbar />
--- apps/spruce/src/hooks/useNetworkStatus.ts
+++ apps/spruce/src/hooks/useNetworkStatus.ts
@@ -1,35 +1,21 @@
 import { useEffect, useState } from "react";
-import { useActivityAnalytics } from "analytics";
-
-type useNetworkStatusType = {
-  (sendAnalytics?: boolean): boolean;
-};
 
 /**
  * This hook sets an eventListener to monitor if the browser is online or offline.
- * @param sendAnalytics - whether or not to send an analytics event when the browser goes online
  * @returns boolean - true if online, false if offline
  */
-export const useNetworkStatus: useNetworkStatusType = (
-  sendAnalytics = false,
-) => {
+export const useNetworkStatus = () => {
   const [isOnline, setIsOnline] = useState(true);
-  const { sendEvent } = useActivityAnalytics();
-
-  const sendOnlineEvent = () => {
-    if (sendAnalytics) {
-      sendEvent({ name: "Tab Active", status: "online" });
-    }
-  };
 
   useEffect(() => {
     const handleOffline = () => {
       // Don't send event because we can't send events if the browser is offline.
       setIsOnline(false);
+      console.log("You are offline.");
     };
     const handleOnline = () => {
-      sendOnlineEvent();
       setIsOnline(true);
+      console.log("You are online.");
     };
     window.addEventListener("offline", handleOffline);
     window.addEventListener("online", handleOnline);
