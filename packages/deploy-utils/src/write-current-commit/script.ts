279334e3940d9cb85463ff4efe1072978e19759c
Sophie Stadler <sophie.stadler@mongodb.com>
2024-06-27 12:37:03 -0400

    Add deploy tasks

--- packages/deploy-utils/src/write-current-commit/script.ts
+++ packages/deploy-utils/src/write-current-commit/script.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env -S vite-node --script
 
-import { writeFileSync } from "fs";
+import { mkdirSync, writeFileSync } from "fs";
 import { getAppToDeploy } from "../utils/environment";
 import { getCurrentlyDeployedCommit } from "../utils/git";
 
 const app = getAppToDeploy();
 const currentlyDeployedCommit = await getCurrentlyDeployedCommit(app);
 
+mkdirSync("bin");
 writeFileSync("bin/previous_deploy.txt", currentlyDeployedCommit);
