41b7c1e394bfd45c6f1b42b604ccb32c43794969
Sophie Stadler <sophie.stadler@mongodb.com>
2024-07-10 13:12:18 -0400

    Simplify command check

--- packages/deploy-utils/src/email/utils.ts
+++ packages/deploy-utils/src/email/utils.ts
@@ -1,5 +1,4 @@
 import { execSync } from "child_process";
-import { accessSync, constants } from "fs";
 
 const pad = (dateOrMonth: number) => dateOrMonth.toString().padStart(2, "0");
 
@@ -11,45 +10,24 @@ const pad = (dateOrMonth: number) => dateOrMonth.toString().padStart(2, "0");
 export const formatDate = (d: Date) =>
   `${d.getFullYear()}-${pad(d.getMonth())}-${pad(d.getDate())}`;
 
-const fileExists = (commandName: string) => {
+const commandExists = (commandName: string) => {
   try {
-    accessSync(commandName, constants.F_OK);
-    return true;
-  } catch (e) {
+    // Prior art from command-exists package
+    // https://github.com/mathisonian/command-exists/blob/742a73d75e6ff737c35aa7c88d0828cbb0455811/lib/command-exists.js#L84-L87
+    const stdout = execSync(
+      `command -v ${commandName} 2>/dev/null && { echo >&1 ${commandName}; exit 0; }`,
+    );
+    return !!stdout;
+  } catch (error) {
     return false;
   }
-};
-
-const localExecutableSync = (commandName: string) => {
-  try {
-    // eslint-disable-next-line no-bitwise
-    accessSync(commandName, constants.F_OK | constants.X_OK);
-    return true;
-  } catch (e) {
-    return false;
-  }
-};
-
-const commandExistsUnixSync = (commandName: string) => {
-  if (!fileExists(commandName)) {
-    console.log("hi");
-    try {
-      const stdout = execSync(
-        `command -v ${commandName} 2>/dev/null  && { echo >&1 ${commandName}; exit 0; }`,
-      );
-      return !!stdout;
-    } catch (error) {
-      return false;
-    }
-  }
-  return localExecutableSync(commandName);
 };
 
 export const findEvergreen = () => {
-  if (commandExistsUnixSync("evergreen")) {
+  if (commandExists("evergreen")) {
     return { evgExecutable: "evergreen", credentials: "" };
   }
-  if (commandExistsUnixSync("~/evergreen")) {
+  if (commandExists("~/evergreen")) {
     return { evgExecutable: "evergreen", credentials: "~/.evergreen.yml" };
   }
   return null;
