eda6a692b9b3acc9e601c6c75640efc2d034df15
sophiabarness <sophia.barnes@mongodb.com>
2024-07-12 16:31:11 -0400

    added Cypress test

--- apps/spruce/cypress/integration/image/navigation.ts
+++ apps/spruce/cypress/integration/image/navigation.ts
@@ -1,7 +1,7 @@
-describe("/image/<imageId>/<random> redirect route", () => {
+describe("/image/imageId/random redirect route", () => {
   it("should redirect to the build information page", () => {
-    cy.visit("/image/<imageId>/random");
+    cy.visit("/image/imageId/random");
     cy.location("pathname").should("not.contain", "/random");
-    cy.location("pathname").should("eq", "/distro/<imageId>/build-information");
+    cy.location("pathname").should("eq", "/image/imageId/build-information");
   });
 });
