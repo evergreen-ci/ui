ce15a67a05e61b9f7c9c14f83f947df867080302
sophiabarness <sophia.barnes@mongodb.com>
2024-07-17 15:08:15 -0400

    changed routing

--- apps/spruce/src/gql/generated/types.ts
+++ apps/spruce/src/gql/generated/types.ts
@@ -849,6 +849,19 @@ export type IceCreamSettingsInput = {
   schedulerHost: Scalars["String"]["input"];
 };
 
+/**
+ * Image is returned by the image query.
+ * It contains information about an image.
+ */
+export type Image = {
+  __typename?: "Image";
+  ami: Scalars["String"]["output"];
+  kernel: Scalars["String"]["output"];
+  lastDeployed: Scalars["Time"]["output"];
+  name: Scalars["String"]["output"];
+  versionId: Scalars["String"]["output"];
+};
+
 export type InstanceTag = {
   __typename?: "InstanceTag";
   canBeModified: Scalars["Boolean"]["output"];
@@ -1989,6 +2002,7 @@ export type Query = {
   host?: Maybe<Host>;
   hostEvents: HostEvents;
   hosts: HostsResponse;
+  image?: Maybe<Image>;
   images: Array<Scalars["String"]["output"]>;
   instanceTypes: Array<Scalars["String"]["output"]>;
   logkeeperBuildMetadata: LogkeeperBuild;
@@ -2081,6 +2095,10 @@ export type QueryHostsArgs = {
   sortDir?: InputMaybe<SortDirection>;
   startedBy?: InputMaybe<Scalars["String"]["input"]>;
   statuses?: InputMaybe<Array<Scalars["String"]["input"]>>;
+};
+
+export type QueryImageArgs = {
+  imageId: Scalars["String"]["input"];
 };
 
 export type QueryLogkeeperBuildMetadataArgs = {
--- apps/spruce/src/pages/image/ImageSelect/index.tsx
+++ apps/spruce/src/pages/image/ImageSelect/index.tsx
@@ -1,5 +1,7 @@
 import { useQuery } from "@apollo/client";
 import { Combobox, ComboboxOption } from "@leafygreen-ui/combobox";
+import { useNavigate } from "react-router-dom";
+import { getImageRoute } from "constants/routes";
 import { zIndex } from "constants/tokens";
 import { ImagesQuery, ImagesQueryVariables } from "gql/generated/types";
 import { IMAGES } from "gql/queries";
@@ -8,7 +10,9 @@ interface ImageSelectProps {
   selectedImage: string;
 }
 
-export const ImageSelect: React.FC<ImageSelectProps> = () => {
+export const ImageSelect: React.FC<ImageSelectProps> = ({ selectedImage }) => {
+  const navigate = useNavigate();
+
   const { data: imagesData, loading } = useQuery<
     ImagesQuery,
     ImagesQueryVariables
@@ -21,9 +25,14 @@ export const ImageSelect: React.FC<ImageSelectProps> = () => {
       clearable={false}
       data-cy="images-select"
       label="Images"
-      placeholder="Images"
+      placeholder="Select an image"
       popoverZIndex={zIndex.popover}
       disabled={loading}
+      // @ts-expect-error: onChange expects type string | null
+      onChange={(imageId: string) => {
+        navigate(getImageRoute(imageId));
+      }}
+      value={selectedImage}
     >
       {images?.map((image) => (
         <ComboboxOption key={image} value={image}>
