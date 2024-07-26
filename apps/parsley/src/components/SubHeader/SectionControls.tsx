1fee65c2c6070993c54c2b00a7e0557dc7e39c7a
Arjun Patel <arjun453@gmail.com>
2024-07-26 15:36:21 -0400

    show button only if it does something

--- apps/parsley/src/components/SubHeader/SectionControls.tsx
+++ apps/parsley/src/components/SubHeader/SectionControls.tsx
@@ -3,18 +3,37 @@ import { useLogContext } from "context/LogContext";
 
 const SectionControls = () => {
   const { sectioning } = useLogContext();
-  const { sectioningEnabled, toggleAllSections } = sectioning;
+  const { sectionState, sectioningEnabled, toggleAllSections } = sectioning;
   if (!sectioningEnabled) {
     return null;
   }
+  const allOpen =
+    sectionState &&
+    Object.values(sectionState).every(
+      (section) =>
+        section.isOpen &&
+        Object.values(section.commands).every((command) => command.isOpen),
+    );
+  const allClosed =
+    sectionState &&
+    Object.values(sectionState).every(
+      (section) =>
+        !section.isOpen &&
+        Object.values(section.commands).every((command) => !command.isOpen),
+    );
+
   return (
     <>
-      <Button onClick={() => toggleAllSections(true)} size="small">
-        Open all sections
-      </Button>
-      <Button onClick={() => toggleAllSections(false)} size="small">
-        Close all sections
-      </Button>
+      {!allOpen && (
+        <Button onClick={() => toggleAllSections(true)} size="small">
+          Open all sections
+        </Button>
+      )}
+      {!allClosed && (
+        <Button onClick={() => toggleAllSections(false)} size="small">
+          Close all sections
+        </Button>
+      )}
     </>
   );
 };
