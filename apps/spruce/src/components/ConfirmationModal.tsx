6a3911eea799c2ee17a6b3d6b7dcec685a76952f
minnakt <minna.kt@mongodb.com>
2024-07-31 20:13:25 -0400

    Fix weird export

--- apps/spruce/src/components/ConfirmationModal.tsx
+++ apps/spruce/src/components/ConfirmationModal.tsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled";
 import Modal, { Variant } from "@leafygreen-ui/confirmation-modal";
 import { zIndex } from "constants/tokens";
 
-export const ConfirmationModal = styled(Modal)`
+const ConfirmationModal = styled(Modal)`
   z-index: ${zIndex.modal};
 `;
-export { Variant };
+export { ConfirmationModal, Variant };
