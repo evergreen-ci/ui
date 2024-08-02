85ff8b6a2b6db5a9ebefb88fabb1061c0b8f21b6
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-08-02 13:26:40 -0400

    Move values into a try catch

--- packages/lib/src/utils/observability/honeycomb.ts
+++ packages/lib/src/utils/observability/honeycomb.ts
@@ -14,44 +14,46 @@ const initializeHoneycomb = ({
   debug,
   serviceName,
 }: HoneycombConfig) => {
-  const userId = localStorage.getItem("userId") ?? undefined;
+  try {
+    const userId = localStorage.getItem("userId") ?? undefined;
+    const honeycombSdk = new HoneycombWebSDK({
+      debug,
+      // endpoint: endpoint,
+      instrumentations: [
+        getWebAutoInstrumentations({
+          "@opentelemetry/instrumentation-fetch": {
+            // Add GraphQL operation name as an attribute to HTTP traces.
+            applyCustomAttributesOnSpan: (span, request) => {
+              if (span && request) {
+                const body = request.body as string;
+                const bodyJson = JSON.parse(body);
+                console.log(bodyJson);
+                const { operationName = "Could not detect operation" } =
+                  bodyJson;
+                span.setAttribute("graphql.operation_name", operationName);
+                console.log(span);
+              }
+            },
 
-  const honeycombSdk = new HoneycombWebSDK({
-    debug,
-
-    // endpoint: endpoint,
-    instrumentations: [
-      getWebAutoInstrumentations({
-        "@opentelemetry/instrumentation-fetch": {
-          // Add GraphQL operation name as an attribute to HTTP traces.
-          applyCustomAttributesOnSpan: (span, request) => {
-            if (span && request) {
-              const body = request.body as string;
-              const bodyJson = JSON.parse(body);
-              console.log(bodyJson);
-              const { operationName = "Could not detect operation" } = bodyJson;
-              span.setAttribute("graphql.operation_name", operationName);
-              console.log(span);
-            }
+            // Allow connecting frontend & backend traces.
+            // propagateTraceHeaderCorsUrls: [new RegExp(getUiUrl())],
           },
+        }),
 
-          // Allow connecting frontend & backend traces.
-          // propagateTraceHeaderCorsUrls: [new RegExp(getUiUrl())],
-        },
-      }),
-
-      new UserInteractionInstrumentation(),
-    ],
-    // Add user.id as an attribute to all traces.
-    resourceAttributes: {
-      "user.id": userId,
-    },
-    localVisualizations: debug,
-    serviceName,
-    apiKey,
-  });
-
-  honeycombSdk.start();
+        new UserInteractionInstrumentation(),
+      ],
+      // Add user.id as an attribute to all traces.
+      resourceAttributes: {
+        "user.id": userId,
+      },
+      localVisualizations: debug,
+      serviceName,
+      apiKey,
+    });
+    honeycombSdk.start();
+  } catch (e) {
+    console.error(`Could not start honeycomb SDK ${e}`);
+  }
 };
 
 export { initializeHoneycomb };
