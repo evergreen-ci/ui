b0319b54b52b6a467af6f428dcccae9956f2e60d
Sophie Stadler <sophie.stadler@mongodb.com>
2024-08-06 17:54:09 -0400

    DEVPROD-3969: Make project settings header sticky (#289)

--- apps/spruce/src/pages/projectSettings/Header.tsx
+++ apps/spruce/src/pages/projectSettings/Header.tsx
@@ -11,19 +11,20 @@ import {
 import { ProjectType } from "./tabs/utils";
 
 interface Props {
+  atTop: boolean;
   id: string;
   projectType: ProjectType;
   tab: ProjectSettingsTabRoutes;
 }
 
-export const Header: React.FC<Props> = ({ id, projectType, tab }) => {
+export const Header: React.FC<Props> = ({ atTop, id, projectType, tab }) => {
   const { title } = getTabTitle(tab);
   const saveable = Object.values(WritableProjectSettingsTabs).includes(
     tab as WritableProjectSettingsType,
   );
 
   return (
-    <Container>
+    <Container atTop={atTop}>
       <H2 data-cy="project-settings-tab-title">{title}</H2>
       {saveable && (
         <HeaderButtons
@@ -36,10 +37,19 @@ export const Header: React.FC<Props> = ({ id, projectType, tab }) => {
   );
 };
 
-const Container = styled.div`
+const Container = styled.div<{ atTop: boolean }>`
   align-items: start;
+  background-color: white;
   display: flex;
   gap: ${size.s};
   justify-content: space-between;
-  margin-bottom: ${size.l};
+  margin: 0 -${size.l};
+  padding: 0 ${size.l} ${size.s} ${size.l};
+  position: sticky;
+  top: 0;
+  z-index: 1;
+
+  ${({ atTop }) =>
+    !atTop &&
+    "box-shadow: 0 0 5px rgba(0, 0, 0, 0.5); clip-path: inset(0px 0px -5px 0px);"}
 `;
--- apps/spruce/src/pages/projectSettings/Tabs.tsx
+++ apps/spruce/src/pages/projectSettings/Tabs.tsx
@@ -37,12 +37,14 @@ type ProjectSettings = ProjectSettingsQuery["projectSettings"];
 type RepoSettings = RepoSettingsQuery["repoSettings"];
 
 interface Props {
+  atTop: boolean;
   projectData?: ProjectSettings;
   projectType: ProjectType;
   repoData?: RepoSettings;
 }
 
 export const ProjectSettingsTabs: React.FC<Props> = ({
+  atTop,
   projectData,
   projectType,
   repoData,
@@ -81,6 +83,7 @@ export const ProjectSettingsTabs: React.FC<Props> = ({
     <Container>
       <NavigationModal />
       <Header
+        atTop={atTop}
         // @ts-expect-error: FIXME. This comment was added by an automated script.
         id={projectId || repoId}
         projectType={projectType}
