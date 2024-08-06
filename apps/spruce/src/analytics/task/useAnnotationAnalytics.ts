e7be253e42152f44965ec5e62378ed7fb6aedbd1
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-08-06 10:40:15 -0400

    Remove unused annotation event data from traces

--- apps/spruce/src/analytics/task/useAnnotationAnalytics.ts
+++ apps/spruce/src/analytics/task/useAnnotationAnalytics.ts
@@ -2,13 +2,8 @@ import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import { useAnalyticsRoot } from "analytics/useAnalyticsRoot";
 import { slugs } from "constants/routes";
-import {
-  BuildBaronQuery,
-  BuildBaronQueryVariables,
-  AnnotationEventDataQuery,
-  AnnotationEventDataQueryVariables,
-} from "gql/generated/types";
-import { ANNOTATION_EVENT_DATA, BUILD_BARON } from "gql/queries";
+import { BuildBaronQuery, BuildBaronQueryVariables } from "gql/generated/types";
+import { BUILD_BARON } from "gql/queries";
 import { useQueryParam } from "hooks/useQueryParam";
 import { RequiredQueryParams } from "types/task";
 
@@ -31,14 +26,6 @@ export const useAnnotationAnalytics = () => {
   const { [slugs.taskId]: taskId } = useParams();
   const [execution] = useQueryParam(RequiredQueryParams.Execution, 0);
 
-  const { data: eventData } = useQuery<
-    AnnotationEventDataQuery,
-    AnnotationEventDataQueryVariables
-  >(ANNOTATION_EVENT_DATA, {
-    variables: { taskId: taskId || "", execution },
-    fetchPolicy: "cache-first",
-  });
-
   const { data: bbData } = useQuery<BuildBaronQuery, BuildBaronQueryVariables>(
     BUILD_BARON,
     {
@@ -47,12 +34,9 @@ export const useAnnotationAnalytics = () => {
     },
   );
 
-  const { annotation } = eventData?.task || {};
   const { buildBaronConfigured } = bbData?.buildBaron || {};
 
   return useAnalyticsRoot<Action>("Annotations", {
-    taskId,
-    annotation,
-    bbConfigured: buildBaronConfigured,
+    "buildbaron.configured": buildBaronConfigured,
   });
 };
