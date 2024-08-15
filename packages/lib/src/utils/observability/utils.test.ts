669ad66f63f39c394af29237e1238bcab5f00b9b
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-08-15 15:29:55 -0400

    Remove unused method

--- packages/lib/src/utils/observability/utils.test.ts
+++ packages/lib/src/utils/observability/utils.test.ts
@@ -1,29 +1,4 @@
-import { getElementName, detectGraphqlQuery } from "./utils";
-
-describe("getElementName", () => {
-  it("returns the data-cy attribute if it exists", () => {
-    const element = document.createElement("button");
-    element.textContent = "test";
-    element.setAttribute("aria-label", "test-aria");
-    element.setAttribute("data-cy", "test-cy");
-    expect(getElementName(element)).toBe(`button[data-cy="test-cy"]`);
-  });
-  it("returns the aria-label attribute if data-cy does not exist", () => {
-    const element = document.createElement("button");
-    element.textContent = "test";
-    element.setAttribute("aria-label", "test-aria");
-    expect(getElementName(element)).toBe(`button[aria-label="test-aria"]`);
-  });
-  it("returns the textContent if neither data-cy nor aria-label exist", () => {
-    const element = document.createElement("button");
-    element.textContent = "test";
-    expect(getElementName(element)).toBe(`button[textContent="test"]`);
-  });
-  it("returns the tagName if none of the above exist", () => {
-    const element = document.createElement("button");
-    expect(getElementName(element)).toBe("button");
-  });
-});
+import { detectGraphqlQuery } from "./utils";
 
 describe("detectGraphqlQuery", () => {
   it("should return the operation name and query type for a valid query", () => {
--- packages/lib/src/utils/observability/utils.ts
+++ packages/lib/src/utils/observability/utils.ts
@@ -1,31 +1,4 @@
 /**
- * `getElementName` is a utility function that gets the name of an HTML element for use with observability tools.
- * It first prefers the `data-cy` attribute, then the text content, and finally the `aria-label` attribute.
- * @param element - The HTML element to compute the name of
- * @returns - The computed name of the element as a string
- */
-const getElementName = (element: Element) => {
-  const tagName = element.tagName.toLowerCase();
-  let attr = element.getAttribute("data-cy");
-  if (attr) {
-    return wrapName(tagName, "data-cy", attr);
-  }
-  attr = element.getAttribute("aria-label");
-  if (attr) {
-    return wrapName(tagName, "aria-label", attr);
-  }
-  attr = element.textContent;
-  if (attr) {
-    return wrapName(tagName, "textContent", attr);
-  }
-
-  return tagName;
-};
-
-const wrapName = (name: string, attribute: string, attributeValue: string) =>
-  `${name}[${attribute}="${attributeValue}"]`;
-
-/**
  * `detectGraphqlQuery` is a utility function that detects the operation name and query type of a GraphQL query.
  * If the body is not a GraphQL query, it returns undefined.
  * @param body - The body of the request
@@ -49,4 +22,4 @@ const detectGraphqlQuery = (
   }
   return undefined;
 };
-export { getElementName, detectGraphqlQuery };
+export { detectGraphqlQuery };
