ebf06ee50c294dde694b0e5274c4ce361ff4a1d9
sophiabarness <80068149+sophiabarness@users.noreply.github.com>
2024-08-19 19:34:05 -0400

    DEVPROD-7656: Create Packages table on Build Information page (#283)

--- apps/spruce/cypress/integration/image/build_information.ts
+++ apps/spruce/cypress/integration/image/build_information.ts
@@ -6,4 +6,49 @@ describe("build information", () => {
       cy.contains("ubuntu1804-workstation");
     });
   });
+
+  describe("packages", () => {
+    it("should show the corresponding packages", () => {
+      cy.visit("/image/ubuntu2204");
+      cy.dataCy("packages-table-row").should("have.length", 10);
+    });
+
+    it("should show different package on new page", () => {
+      cy.visit("/image/ubuntu2204");
+      cy.dataCy("packages-table-row").should("have.length", 10);
+
+      // First package name on first page.
+      cy.dataCy("packages-table-row")
+        .first()
+        .find("td")
+        .first()
+        .invoke("text")
+        .as("firstPackageName", { type: "static" });
+
+      cy.get("[data-testid=lg-pagination-next-button]").click();
+
+      // First package name on second page.
+      cy.dataCy("packages-table-row")
+        .first()
+        .find("td")
+        .first()
+        .invoke("text")
+        .as("nextPackageName", { type: "static" });
+
+      // Package names should not be equal.
+      cy.get("@firstPackageName").then((firstPackageName) => {
+        cy.get("@nextPackageName").then((nextPackageName) => {
+          expect(nextPackageName).not.to.equal(firstPackageName);
+        });
+      });
+    });
+
+    it("should show no packages when bogus filter is applied", () => {
+      cy.visit("/image/ubuntu2204");
+      cy.dataCy("packages-table-row").should("have.length", 10);
+      cy.dataCy("package-name-filter").click();
+      cy.get('input[placeholder="Search name"]').type("boguspackage{enter}");
+      cy.dataCy("packages-table-row").should("have.length", 0);
+    });
+  });
 });
