1d4f6dd01423b3b90804a08ab0e4df6544518044
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-08-23 16:15:02 -0400

    Update Honeycomb SDK initialization to handle missing API key

--- packages/lib/src/utils/observability/honeycomb.ts
+++ packages/lib/src/utils/observability/honeycomb.ts
@@ -15,51 +15,63 @@ const initializeHoneycomb = ({
   debug,
   serviceName,
 }: HoneycombConfig) => {
-  try {
-    const userId = localStorage.getItem("userId") ?? undefined;
-    const honeycombSdk = new HoneycombWebSDK({
-      debug,
-      // endpoint: endpoint,
-      instrumentations: [
-        getWebAutoInstrumentations({
-          "@opentelemetry/instrumentation-fetch": {
-            // Add GraphQL operation name as an attribute to HTTP traces.
-            applyCustomAttributesOnSpan: (span, request) => {
-              if (span && request) {
-                const body = request.body as string;
-                const graphqlQuery = detectGraphqlQuery(body);
-                if (graphqlQuery) {
-                  span.setAttribute(
-                    "graphql.operation_name",
-                    graphqlQuery.operationName,
-                  );
-                  span.setAttribute(
-                    "graphql.query_type",
-                    graphqlQuery.queryType,
-                  );
+  if (debug && !apiKey) {
+    console.warn(
+      "Honeycomb API key was not provided. Starting SDK in debug mode.",
+    );
+    return;
+  }
+  if (!apiKey) {
+    console.error(
+      "Honeycomb API key is required to start the SDK in production mode.",
+    );
+  } else {
+    try {
+      const userId = localStorage.getItem("userId") ?? undefined;
+      const honeycombSdk = new HoneycombWebSDK({
+        debug,
+        // endpoint: endpoint,
+        instrumentations: [
+          getWebAutoInstrumentations({
+            "@opentelemetry/instrumentation-fetch": {
+              // Add GraphQL operation name as an attribute to HTTP traces.
+              applyCustomAttributesOnSpan: (span, request) => {
+                if (span && request) {
+                  const body = request.body as string;
+                  const graphqlQuery = detectGraphqlQuery(body);
+                  if (graphqlQuery) {
+                    span.setAttribute(
+                      "graphql.operation_name",
+                      graphqlQuery.operationName,
+                    );
+                    span.setAttribute(
+                      "graphql.query_type",
+                      graphqlQuery.queryType,
+                    );
+                  }
                 }
-              }
-            },
+              },
 
-            // Allow connecting frontend & backend traces.
-            propagateTraceHeaderCorsUrls: [backendURL || ""],
-          },
-          "@opentelemetry/instrumentation-document-load": {
-            ignoreNetworkEvents: true,
-          },
-        }),
-      ],
-      // Add user.id as an attribute to all traces.
-      resourceAttributes: {
-        "user.id": userId,
-      },
-      localVisualizations: debug,
-      serviceName,
-      apiKey,
-    });
-    honeycombSdk.start();
-  } catch (e) {
-    console.error(`Could not start Honeycomb SDK: ${e}`);
+              // Allow connecting frontend & backend traces.
+              propagateTraceHeaderCorsUrls: [backendURL || ""],
+            },
+            "@opentelemetry/instrumentation-document-load": {
+              ignoreNetworkEvents: true,
+            },
+          }),
+        ],
+        // Add user.id as an attribute to all traces.
+        resourceAttributes: {
+          "user.id": userId,
+        },
+        localVisualizations: debug,
+        serviceName,
+        apiKey,
+      });
+      honeycombSdk.start();
+    } catch (e) {
+      console.error(`Could not start Honeycomb SDK: ${e}`);
+    }
   }
 };
 
