12bbf1184dea0b3fbfad6fec4f2cd1b4cad10066
minnakt <minna.kt@mongodb.com>
2024-08-27 13:40:38 -0400

    DEVPROD-8865: Upgrade Vitest to v2.0.5

--- apps/spruce/vite.config.ts
+++ apps/spruce/vite.config.ts
@@ -152,7 +152,6 @@ export default defineConfig({
     globals: true,
     globalSetup: "./config/vitest/global-setup.ts",
     outputFile: { junit: "./bin/vitest/junit.xml" },
-    pool: "forks", // https://vitest.dev/guide/common-errors.html#failed-to-terminate-worker
     reporters: ["default", ...(process.env.CI === "true" ? ["junit"] : [])],
     setupFiles: "./config/vitest/setupTests.ts",
   },
