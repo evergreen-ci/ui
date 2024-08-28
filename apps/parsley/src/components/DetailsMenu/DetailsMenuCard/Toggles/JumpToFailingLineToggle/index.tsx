1a0fa6a7041141e296f0b3a1dfcfbf02faecf1c4
Arjun Patel <arjun453@gmail.com>
2024-08-28 14:47:52 -0400

    add guid cues

--- apps/parsley/src/components/DetailsMenu/DetailsMenuCard/Toggles/JumpToFailingLineToggle/index.tsx
+++ apps/parsley/src/components/DetailsMenu/DetailsMenuCard/Toggles/JumpToFailingLineToggle/index.tsx
@@ -1,6 +1,9 @@
+import { useRef } from "react";
+import { GuideCue } from "@leafygreen-ui/guide-cue";
 import { usePreferencesAnalytics } from "analytics";
 import { LogTypes } from "constants/enums";
 import { useLogContext } from "context/LogContext";
+import { useSectionsFeatureDiscoveryContext } from "context/SectionsFeatureDiscoveryContext";
 import { ParsleySettingsInput } from "gql/generated/types";
 import BaseToggle from "../BaseToggle";
 
@@ -16,19 +19,37 @@ const JumpToFailingLineToggle: React.FC<JumpToFailingLineToggleProps> = ({
   const { sendEvent } = usePreferencesAnalytics();
   const { logMetadata } = useLogContext();
   const isTaskLog = logMetadata?.logType === LogTypes.EVERGREEN_TASK_LOGS;
-
+  const { closeSecondGuideCue, isOpenSecondGuideCue, setIsOpenSecondGuideCue } =
+    useSectionsFeatureDiscoveryContext();
+  const triggerRef = useRef<HTMLDivElement>(null);
   return (
-    <BaseToggle
-      data-cy="jump-to-failing-line-toggle"
-      disabled={!isTaskLog}
-      label="Jump to Failing Line"
-      onChange={(value) => {
-        updateSettings({ jumpToFailingLineEnabled: value });
-        sendEvent({ name: "Toggled jump to failing line", on: value });
-      }}
-      tooltip="Automatically scroll to the failing log line on page load. Only available for task logs."
-      value={checked}
-    />
+    <>
+      <GuideCue
+        beaconAlign="right"
+        data-cy="sectioning-cue-2"
+        numberOfSteps={1}
+        onPrimaryButtonClick={closeSecondGuideCue}
+        open={isOpenSecondGuideCue}
+        refEl={triggerRef}
+        setOpen={setIsOpenSecondGuideCue}
+        title="Jump to Failing Line"
+      >
+        Combined with sectioning, jump to failing line allows to streamline your
+        failure triage process.
+      </GuideCue>
+      <BaseToggle
+        ref={triggerRef}
+        data-cy="jump-to-failing-line-toggle"
+        disabled={!isTaskLog}
+        label="Jump to Failing Line"
+        onChange={(value) => {
+          updateSettings({ jumpToFailingLineEnabled: value });
+          sendEvent({ name: "Toggled jump to failing line", on: value });
+        }}
+        tooltip="Automatically scroll to the failing log line on page load. Only available for task logs."
+        value={checked}
+      />
+    </>
   );
 };
 
--- apps/parsley/src/components/DetailsMenu/DetailsMenuCard/Toggles/SectionsToggle/index.tsx
+++ apps/parsley/src/components/DetailsMenu/DetailsMenuCard/Toggles/SectionsToggle/index.tsx
@@ -1,6 +1,9 @@
+import { useRef } from "react";
+import { GuideCue } from "@leafygreen-ui/guide-cue";
 import { usePreferencesAnalytics } from "analytics";
 import { LogTypes } from "constants/enums";
 import { useLogContext } from "context/LogContext";
+import { useSectionsFeatureDiscoveryContext } from "context/SectionsFeatureDiscoveryContext";
 import { ParsleySettingsInput } from "gql/generated/types";
 import BaseToggle from "../BaseToggle";
 
@@ -17,21 +20,41 @@ const SectionsToggle: React.FC<SectionsToggleProps> = ({
   const { logMetadata } = useLogContext();
 
   const isTaskLog = logMetadata?.logType === LogTypes.EVERGREEN_TASK_LOGS;
-
+  const { closeFirstGuideCue, isOpenFirstGuideCue, setIsOpenFirstGuideCue } =
+    useSectionsFeatureDiscoveryContext();
+  const triggerRef = useRef<HTMLDivElement>(null);
   return (
-    <BaseToggle
-      data-cy="sections-toggle"
-      disabled={!isTaskLog}
-      label="Sections"
-      onChange={(value) => {
-        updateSettings({
-          sectionsEnabled: value,
-        });
-        sendEvent({ name: "Toggled sections", on: value });
-      }}
-      tooltip="Toggle sections. Only available for Evergreen task logs."
-      value={checked}
-    />
+    <>
+      <GuideCue
+        currentStep={1}
+        data-cy="sectioning-cue-1"
+        numberOfSteps={1}
+        onPrimaryButtonClick={closeFirstGuideCue}
+        open={isOpenFirstGuideCue}
+        refEl={triggerRef}
+        setOpen={setIsOpenFirstGuideCue}
+        title="Opt-In to Sectioned Task Logs"
+        tooltipAlign="bottom"
+        tooltipJustify="end"
+      >
+        This beta feature is now available for task logs. Please send over any
+        feedback to the #ask-devprod-evergreen channel.
+      </GuideCue>
+      <BaseToggle
+        ref={triggerRef}
+        data-cy="sections-toggle"
+        disabled={!isTaskLog}
+        label="Sections (Beta)"
+        onChange={(value) => {
+          updateSettings({
+            sectionsEnabled: value,
+          });
+          sendEvent({ name: "Toggled sections", on: value });
+        }}
+        tooltip="Toggle sections. Only available for Evergreen task logs."
+        value={checked}
+      />
+    </>
   );
 };
 
