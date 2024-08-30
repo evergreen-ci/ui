410c24349bf62a7a81dc96f1d8ed4bef3f5b8184
minnakt <47064971+minnakt@users.noreply.github.com>
2024-08-30 09:52:32 -0400

    DEVPROD-7054: Omit changes tab for GitHub merge queue patches (#347)

--- apps/spruce/src/pages/Version.tsx
+++ apps/spruce/src/pages/Version.tsx
@@ -160,7 +160,6 @@ export const VersionPage: React.FC = () => {
   } = version || {};
   const {
     canEnqueueToCommitQueue,
-    childPatches,
     commitQueuePosition = null,
     patchNumber,
   } = patch || {};
@@ -229,11 +228,8 @@ export const VersionPage: React.FC = () => {
         <PageLayout>
           <PageContent>
             <VersionTabs
-              childPatches={childPatches}
               // @ts-expect-error: FIXME. This comment was added by an automated script.
-              isPatch={version?.isPatch}
-              // @ts-expect-error: FIXME. This comment was added by an automated script.
-              taskCount={version?.taskCount}
+              version={version}
             />
           </PageContent>
         </PageLayout>
