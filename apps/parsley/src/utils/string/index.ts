7d8fb0a223039dc0cd50eaa943a5e87121b55a2f
Arjun Patel <arjun453@gmail.com>
2024-09-16 15:26:45 -0400

    cr

--- apps/parsley/src/utils/string/index.ts
+++ apps/parsley/src/utils/string/index.ts
@@ -135,14 +135,13 @@ export const trimSeverity = (line: string) => {
  * @returns true if line is the failing log line, false otherwise
  */
 export const isFailingLine = (line: string, failingCommand: string) => {
-  console.log(failingCommand);
   const failedExpression = `${failingCommand} failed`;
   const timeoutExpression = `${failingCommand} stopped early`;
   // The "Finished" command is matched when the task status is set to fail by the user.
-  const finished = `Finished command ${failingCommand}`;
+  const finishedExpression = `Finished command ${failingCommand}`;
   return (
     line.includes(failedExpression) ||
     line.includes(timeoutExpression) ||
-    line.includes(finished)
+    line.includes(finishedExpression)
   );
 };
