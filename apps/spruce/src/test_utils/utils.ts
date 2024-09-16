3d83d7242c84a9c9836b8ea3ea43a4bf6613f802
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-09-16 13:10:48 -0400

    Fix selectLGOption to select option by text instead of data-cy

--- apps/spruce/src/test_utils/utils.ts
+++ apps/spruce/src/test_utils/utils.ts
@@ -3,14 +3,14 @@ import { screen, userEvent } from "@evg-ui/lib/test_utils";
 /**
  * `selectLGOption` selects an option from a LG select component
  * @param dataCy - data-cy selector of the LG select component
- * @param option - data-cy selector of the option to select
+ * @param option - text contents of the option to select
  */
 const selectLGOption = async (dataCy: string, option: string) => {
   const user = userEvent.setup();
   expect(screen.queryByDataCy(dataCy)).not.toBeDisabled();
   await user.click(screen.getByDataCy(dataCy));
-  await screen.findByText(option);
-  await user.click(screen.getByDataCy(option));
+  const selectOption = await screen.findByText(option);
+  await user.click(selectOption);
   expect(screen.queryByDataCy(dataCy)).toHaveTextContent(option);
 };
 
