da47788e950b1867fbee55952bd579381eb4f281
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-09-18 18:04:08 -0400

    Fix tsconfig for allowing vitest matchers for lib tests

---
+++ packages/lib/config/vitest/setupTests.ts
@@ -0,0 +1,21 @@
+// jest-dom adds custom matchers for asserting on DOM nodes. Works for Vitest too!
+import "@testing-library/jest-dom";
+import "vitest-canvas-mock";
+
+// @ts-ignore
+window.crypto.randomUUID = (() => {
+  let value = 0;
+  return () => {
+    value += 1;
+    return value.toString();
+  };
+})();
+
+// Workaround for a bug in @testing-library/react.
+// It prevents Vitest's fake timers from functioning with user-event.
+// https://github.com/testing-library/react-testing-library/issues/1197
+// @ts-expect-error
+globalThis.jest = {
+  ...globalThis.jest,
+  advanceTimersByTime: vi.advanceTimersByTime.bind(vi),
+};
