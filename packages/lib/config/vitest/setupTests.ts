8c670f62c9572998ddbe4447ae4a56c82abb2f0f
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-09-23 12:49:53 -0400

    Bump types

--- packages/lib/config/vitest/setupTests.ts
+++ packages/lib/config/vitest/setupTests.ts
@@ -2,15 +2,6 @@
 import "@testing-library/jest-dom";
 import "vitest-canvas-mock";
 
-// @ts-ignore
-window.crypto.randomUUID = (() => {
-  let value = 0;
-  return () => {
-    value += 1;
-    return value.toString();
-  };
-})();
-
 // Workaround for a bug in @testing-library/react.
 // It prevents Vitest's fake timers from functioning with user-event.
 // https://github.com/testing-library/react-testing-library/issues/1197
