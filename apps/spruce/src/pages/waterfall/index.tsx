18b298f0af9af3db042d1677bc4c9a9d1322bf11
Sophie Stadler <sophie.stadler@mongodb.com>
2024-09-25 11:00:36 -0400

    Fix Safari performance

--- apps/spruce/src/pages/waterfall/index.tsx
+++ apps/spruce/src/pages/waterfall/index.tsx
@@ -1,13 +1,39 @@
 import { Suspense } from "react";
-import { PageWrapper } from "components/styles";
+import { Global, css } from "@emotion/react";
+import styled from "@emotion/styled";
+import { navBarHeight } from "components/Header/Navbar";
+import { size } from "constants/tokens";
 import { WaterfallGrid } from "./WaterfallGrid";
 
 const Waterfall: React.FC = () => (
-  <PageWrapper data-cy="waterfall-page">
-    <Suspense fallback={<h3>Loading waterfall...</h3>}>
-      <WaterfallGrid />
-    </Suspense>
-  </PageWrapper>
+  <>
+    {/* Safari performance of the waterfall chokes if using overflow-y: scroll, so we need to scroll the page to scroll instead.
+    Update navbar layout to accomodate this. */}
+    <Global
+      styles={css`
+        header {
+          position: unset !important;
+        }
+        #banner-container {
+          margin-top: calc(${navBarHeight} + ${size.s});
+        }
+        nav {
+          position: fixed !important;
+          width: 100%;
+          z-index: 1;
+        }
+      `}
+    />
+    <PageContainer data-cy="waterfall-page">
+      <Suspense fallback={<h3>Loading waterfall...</h3>}>
+        <WaterfallGrid />
+      </Suspense>
+    </PageContainer>
+  </>
 );
+
+const PageContainer = styled.div`
+  padding: ${size.m} ${size.l};
+`;
 
 export default Waterfall;
