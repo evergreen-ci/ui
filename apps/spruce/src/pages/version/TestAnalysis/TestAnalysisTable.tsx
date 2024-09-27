a53325de4ea7d216251d8fe6bdaa37057fa35292
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-09-27 15:21:00 -0400

    Revert "Remove linkification and add statuses badges"
    This reverts commit 0b090edb5383922c1bd5e8f75f62d5612e9a22e5.

--- apps/spruce/src/pages/version/TestAnalysis/TestAnalysisTable.tsx
+++ apps/spruce/src/pages/version/TestAnalysis/TestAnalysisTable.tsx
@@ -1,8 +1,10 @@
 import { useRef, useMemo } from "react";
 import Card from "@leafygreen-ui/card";
 import { useLeafyGreenTable, LGColumnDef } from "@leafygreen-ui/table";
-import TaskStatusBadge from "@evg-ui/lib/components/Badge/TaskStatusBadge";
+import { StyledRouterLink } from "components/styles";
 import { BaseTable } from "components/Table/BaseTable";
+import { getTaskRoute } from "constants/routes";
+import { TaskTab } from "types/task";
 import FailedTestGroupTable from "./FailedTestGroup/FailedTestGroupTable";
 import { TaskBuildVariantField } from "./types";
 
@@ -19,7 +21,6 @@ const TestAnalysisTable: React.FC<TestAnalysisTableProps> = ({ tasks }) => {
     () =>
       Object.entries(tasks).map(([, t]) => ({
         testName: t[0],
-        statuses: countStatuses(t[1]),
         id: t[1][0].id,
         renderExpandedContent: () => (
           <Card>
@@ -29,57 +30,30 @@ const TestAnalysisTable: React.FC<TestAnalysisTableProps> = ({ tasks }) => {
       })),
     [tasks],
   );
-
   const table = useLeafyGreenTable<TableField>({
     columns,
     containerRef: tableContainerRef,
     data: tableData,
-
     defaultColumn: {
       enableColumnFilter: false,
     },
   });
-  return <BaseTable shouldAlternateRowColor table={table} />;
+  return <BaseTable table={table} />;
 };
 
 const columns: LGColumnDef<TableField>[] = [
   {
     header: "Test Name",
     accessorKey: "testName",
-    cell: ({ getValue }) => getValue() as string,
+    cell: ({ getValue, row }) => (
+      <StyledRouterLink
+        to={getTaskRoute(row.original.id, { tab: TaskTab.Tests })}
+      >
+        {getValue()}
+      </StyledRouterLink>
+    ),
     enableSorting: true,
   },
-  {
-    header: "Statuses",
-    accessorKey: "statuses",
-    cell: ({ getValue }) =>
-      Object.entries(getValue() as Map<string, number>).map(
-        ([status, count]) => (
-          <TaskStatusBadge key={status} status={status} taskCount={count} />
-        ),
-      ),
-  },
 ];
-
-/**
- * `countStatuses` counts the number of tasks with each status
- * @param tasks - list of tasks
- * @returns - object with status as key and count as value
- */
-const countStatuses = (tasks: TaskBuildVariantField[]) => {
-  const statuses = tasks.reduce(
-    (acc, task) => {
-      const { status } = task;
-      if (acc[status]) {
-        acc[status] += 1;
-      } else {
-        acc[status] = 1;
-      }
-      return acc;
-    },
-    {} as Record<string, number>,
-  );
-  return statuses;
-};
 
 export default TestAnalysisTable;
