8ad60ab2b05e572c3f0913e87c35652b3a8751a3
SupaJoon <arjun453@gmail.com>
2024-10-09 13:20:37 -0400

    DEVPROD-5305: Introduce Sections Official Release feature modal [minor] (#411)

--- apps/parsley/cypress/support/index.ts
+++ apps/parsley/cypress/support/index.ts
@@ -114,8 +114,7 @@ declare global {
     cy.login();
     cy.setCookie("has-opened-drawer", "true");
     cy.setCookie("has-seen-searchbar-guide-cue", "true");
-    cy.setCookie("has-seen-sections-beta-guide-cue", "true");
-    cy.setCookie("has-seen-sections-beta-feature-modal", "true");
+    cy.setCookie("has-seen-sections-prod-feature-modal", "true");
     mutationDispatched = false;
     cy.intercept("POST", "/graphql/query", (req) => {
       const isMutation = req.body.query?.startsWith("mutation");
