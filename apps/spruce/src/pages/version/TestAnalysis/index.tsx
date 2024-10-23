76ad8bee3ae3c061b223a4cb9bcd3d1efbab82a9
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-10-23 09:22:33 -0400

    Consolidate filter components into their own file

--- apps/spruce/src/pages/version/TestAnalysis/index.tsx
+++ apps/spruce/src/pages/version/TestAnalysis/index.tsx
@@ -1,56 +1,46 @@
 import { useMemo } from "react";
 import { useQuery } from "@apollo/client";
 import styled from "@emotion/styled";
-import Button from "@leafygreen-ui/button";
-import { Combobox, ComboboxOption } from "@leafygreen-ui/combobox";
 import { BasicEmptyState } from "@leafygreen-ui/empty-state";
 import { palette } from "@leafygreen-ui/palette";
 import { ListSkeleton } from "@leafygreen-ui/skeleton-loader";
-import { H3, Body, H3Props, Disclaimer } from "@leafygreen-ui/typography";
+import { H3, Body, H3Props } from "@leafygreen-ui/typography";
 import pluralize from "pluralize";
 import { size } from "@evg-ui/lib/constants/tokens";
-import { useVersionAnalytics } from "analytics";
-import TextInputWithValidation from "components/TextInputWithValidation";
-import { failedTaskStatuses, taskStatusToCopy } from "constants/task";
+import { failedTaskStatuses } from "constants/task";
 import { useToastContext } from "context/toast";
 import {
   TestAnalysisQuery,
   TestAnalysisQueryVariables,
 } from "gql/generated/types";
 import { TEST_ANALYSIS } from "gql/queries";
-import { useQueryParam, useQueryParams } from "hooks/useQueryParam";
+import { useQueryParam } from "hooks/useQueryParam";
 import { TestAnalysisQueryParams } from "types/task";
 import { reportError } from "utils/errorReporting";
-import { validateRegexp } from "utils/validators";
+import FilterGroup from "./FilterGroup";
 import GroupedTestMapList from "./GroupedTestMapList";
 import { TaskBuildVariantField } from "./types";
-import {
-  countTotalTests,
-  filterGroupedTests,
-  getAllBuildVariants,
-  getAllTaskStatuses,
-  groupTestsByName,
-} from "./utils";
+import { countTotalTests, filterGroupedTests, groupTestsByName } from "./utils";
 
 const { green } = palette;
 interface TestAnalysisProps {
   versionId: string;
 }
 const TestAnalysis: React.FC<TestAnalysisProps> = ({ versionId }) => {
-  const [selectedTaskStatuses, setSelectedTaskStatuses] = useQueryParam<
-    string[]
-  >(TestAnalysisQueryParams.Statuses, []);
+  const [selectedTaskStatuses] = useQueryParam<string[]>(
+    TestAnalysisQueryParams.Statuses,
+    [],
+  );
 
-  const [selectedBuildVariants, setSelectedBuildVariants] = useQueryParam<
-    string[]
-  >(TestAnalysisQueryParams.Variants, []);
+  const [selectedBuildVariants] = useQueryParam<string[]>(
+    TestAnalysisQueryParams.Variants,
+    [],
+  );
 
-  const [testName, setTestName] = useQueryParam<string | undefined>(
+  const [testName] = useQueryParam<string | undefined>(
     TestAnalysisQueryParams.TestName,
     "",
   );
-  const [, setQueryParams] = useQueryParams();
-  const { sendEvent } = useVersionAnalytics(versionId);
 
   const dispatchToast = useToastContext();
   const { data, loading } = useQuery<
@@ -76,14 +66,6 @@ const TestAnalysis: React.FC<TestAnalysisProps> = ({ versionId }) => {
     [data],
   );
 
-  const buildVariants = useMemo(
-    () => getAllBuildVariants(groupedTestsMap),
-    [groupedTestsMap],
-  );
-  const taskStatuses = useMemo(
-    () => getAllTaskStatuses(groupedTestsMap),
-    [groupedTestsMap],
-  );
   let filteredGroupedTestsMap = new Map<string, TaskBuildVariantField[]>();
   try {
     filteredGroupedTestsMap = filterGroupedTests(
@@ -109,10 +91,7 @@ const TestAnalysis: React.FC<TestAnalysisProps> = ({ versionId }) => {
 
   const hasMatchingResults = totalFilteredTestCount > 0;
   const hasResults = Boolean(data) && totalTestCount > 0;
-  const hasFiltersApplied =
-    selectedTaskStatuses.length > 0 ||
-    selectedBuildVariants.length > 0 ||
-    (testName || "").length > 0;
+
   return (
     <Container>
       {loading ? (
@@ -129,99 +108,13 @@ const TestAnalysis: React.FC<TestAnalysisProps> = ({ versionId }) => {
             version. If a test fails across multiple tasks, it could suggest a
             flaky test or a broader issue.
           </Body>
-          <FilterContainer>
-            <TextInputWithValidation
-              aria-labelledby="test-failure-search-label"
-              defaultValue={testName}
-              disabled={!hasResults}
-              id="test-failure-search-input"
-              label="Search Test Failures"
-              onSubmit={(value) => {
-                if (value === "") {
-                  setTestName(undefined);
-                } else {
-                  setTestName(value);
-                }
-                sendEvent({
-                  name: "Filtered test analysis tab",
-                  "filter.by": "test name",
-                });
-              }}
-              placeholder="Search failed tests (regex)"
-              validator={validateRegexp}
-            />
-            <Combobox
-              disabled={!hasResults}
-              label="Failure Type"
-              multiselect
-              onChange={(value: string[]) => {
-                setSelectedTaskStatuses(value);
-                sendEvent({
-                  name: "Filtered test analysis tab",
-                  "filter.by": "task statuses",
-                });
-              }}
-              placeholder="Select a task status"
-              value={selectedTaskStatuses}
-            >
-              {taskStatuses.map((status) => (
-                <ComboboxOption
-                  key={status}
-                  data-cy={`task-status-${status}-option`}
-                  displayName={taskStatusToCopy[status]}
-                  value={status}
-                />
-              ))}
-            </Combobox>
-            <Combobox
-              disabled={!hasResults}
-              label="Build Variant"
-              multiselect
-              onChange={(value: string[]) => {
-                setSelectedBuildVariants(value);
-                sendEvent({
-                  name: "Filtered test analysis tab",
-                  "filter.by": "build variant",
-                });
-              }}
-              overflow="scroll-x"
-              placeholder="Select a build variant"
-              value={selectedBuildVariants}
-            >
-              {buildVariants.map((bv) => (
-                <ComboboxOption
-                  key={bv.buildVariant}
-                  data-cy={`build-variant-${bv.buildVariant}-option`}
-                  displayName={bv.buildVariantDisplayName}
-                  value={bv.buildVariant}
-                />
-              ))}
-            </Combobox>
-          </FilterContainer>
-          {hasResults && (
-            <FilterSubheaderContainer>
-              <Body weight="medium">
-                {totalFilteredTestCount}/{totalTestCount} Failed{" "}
-                {pluralize("Test", totalFilteredTestCount)}
-              </Body>
-              <Button
-                data-cy="clear-filter-button"
-                disabled={!hasFiltersApplied}
-                onClick={() => {
-                  setQueryParams({
-                    [TestAnalysisQueryParams.Statuses]: [],
-                    [TestAnalysisQueryParams.Variants]: [],
-                    [TestAnalysisQueryParams.TestName]: "",
-                  });
-                }}
-                size="xsmall"
-              >
-                Clear Filters
-              </Button>
-              {hasFiltersApplied && <Disclaimer>Filters applied</Disclaimer>}
-            </FilterSubheaderContainer>
-          )}
-
+          <FilterGroup
+            groupedTestsMap={groupedTestsMap}
+            hasResults={hasResults}
+            totalFilteredTestCount={totalFilteredTestCount}
+            totalTestCount={totalTestCount}
+            versionId={versionId}
+          />
           <GroupedTestMapList groupedTestsMapEntries={groupedTestsMapEntries} />
           {!hasMatchingResults && (
             <BasicEmptyState
@@ -238,23 +131,6 @@ const TestAnalysis: React.FC<TestAnalysisProps> = ({ versionId }) => {
 const Container = styled.div`
   margin-right: ${size.xl};
   margin-left: ${size.xxs};
-`;
-
-const FilterSubheaderContainer = styled.div`
-  display: flex;
-  margin-bottom: ${size.m};
-  gap: ${size.xs};
-  align-items: center;
-`;
-
-const FilterContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: ${size.m};
-  margin-top: ${size.s};
-  > * {
-    flex-basis: 30%;
-  }
 `;
 
 const Title = styled(H3)<H3Props>`
--- apps/spruce/src/pages/version/TestAnalysis/utils.ts
+++ apps/spruce/src/pages/version/TestAnalysis/utils.ts
@@ -1,4 +1,3 @@
-import { TaskStatus } from "@evg-ui/lib/types/task";
 import {
   TestAnalysisQueryTasks,
   TaskBuildVariantField,
@@ -83,52 +82,6 @@ const filterGroupedTests = (
   return filteredTests;
 };
 
-type BuildVariantNameTuple = {
-  buildVariant: string;
-  buildVariantDisplayName: string;
-};
-/**
- * `getAllBuildVariants` extracts all unique buildVariants from a map of test names to TaskBuildVariantField arrays.
- * @param taskMap - A Map of test names to an array of TaskBuildVariantField objects.
- * @returns - An array of unique buildVariants.
- */
-const getAllBuildVariants = (
-  taskMap: Map<string, TaskBuildVariantField[]>,
-): BuildVariantNameTuple[] => {
-  const buildVariantMap = new Map<string, string>();
-
-  taskMap.forEach((taskArray) => {
-    taskArray.forEach((task) => {
-      const displayName = task.buildVariantDisplayName || task.buildVariant;
-      buildVariantMap.set(task.buildVariant, displayName);
-    });
-  });
-
-  return Array.from(buildVariantMap.entries()).map(
-    ([buildVariant, buildVariantDisplayName]) => ({
-      buildVariant,
-      buildVariantDisplayName,
-    }),
-  );
-};
-
-/**
- * `getAllTaskStatuses` extracts all unique statuses from a map of test names to TaskBuildVariantField arrays.
- * @param taskMap - A Map of test names to an array of TaskBuildVariantField objects.
- * @returns - An array of unique statuses.
- */
-const getAllTaskStatuses = (taskMap: Map<string, TaskBuildVariantField[]>) => {
-  const statusSet = new Set<TaskStatus>();
-
-  taskMap.forEach((taskArray) => {
-    taskArray.forEach((task) => {
-      statusSet.add(task.status as TaskStatus);
-    });
-  });
-
-  return Array.from(statusSet);
-};
-
 /**
  * `countTotalTests` counts the total number of tests accounting for a task running on multiple tasks and build variants.
  * @param taskMap - A Map of test names to an array of TaskBuildVariantField objects.
@@ -143,10 +96,4 @@ const countTotalTests = (taskMap: Map<string, TaskBuildVariantField[]>) => {
   return totalTests;
 };
 
-export {
-  countTotalTests,
-  groupTestsByName,
-  filterGroupedTests,
-  getAllBuildVariants,
-  getAllTaskStatuses,
-};
+export { countTotalTests, groupTestsByName, filterGroupedTests };
