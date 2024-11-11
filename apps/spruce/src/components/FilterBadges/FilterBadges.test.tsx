7457607a195410eac46481a7da227d8ac594a6eb
Sophie Stadler <sophie.stadler@mongodb.com>
2024-11-11 11:15:06 -0500

    fix tests

--- apps/spruce/src/components/FilterBadges/FilterBadges.test.tsx
+++ apps/spruce/src/components/FilterBadges/FilterBadges.test.tsx
@@ -123,7 +123,7 @@ describe("filterBadges", () => {
         onRemove={vi.fn()}
       />,
     );
-    await user.click(screen.getByRole("button", { name: "Clear all filters" }));
+    await user.click(screen.getByRole("button", { name: "CLEAR ALL FILTERS" }));
     expect(onClearAll).toHaveBeenCalledTimes(1);
   });
 
