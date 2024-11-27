280f945afbab8b4999780468915b7a43440d571a
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-11-27 13:01:26 -0500

    Remove ts-expect-error

--- apps/spruce/src/components/styles/StyledTabs.tsx
+++ apps/spruce/src/components/styles/StyledTabs.tsx
@@ -2,7 +2,6 @@ import styled from "@emotion/styled";
 import { Tabs, TabsProps } from "@leafygreen-ui/tabs";
 import { size } from "@evg-ui/lib/constants/tokens";
 
-// @ts-expect-error
 export const StyledTabs = styled(Tabs)<TabsProps>`
   > div > [role="tablist"] {
     margin-bottom: ${size.s};
