cbe993a59ba741f369c7a562201af594dfd64d4b
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-01-24 11:21:49 -0500

    Set some prop types

--- apps/parsley/src/components/LoadingBar/LoadingBar.stories.tsx
+++ apps/parsley/src/components/LoadingBar/LoadingBar.stories.tsx
@@ -1,7 +1,19 @@
+import { CustomMeta, CustomStoryObj } from "@evg-ui/lib/test_utils/types";
 import LoadingBar from ".";
 
 export default {
+  argTypes: {
+    indeterminate: {
+      control: { type: "boolean" },
+    },
+    progress: {
+      control: { max: 100, min: 0, step: 1, type: "range" },
+    },
+  },
+  args: {
+    indeterminate: true,
+  },
   component: LoadingBar,
-};
+} satisfies CustomMeta<typeof LoadingBar>;
 
-export const Default = {};
+export const Default: CustomStoryObj<typeof LoadingBar> = {};
