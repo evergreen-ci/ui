66815a72ffff1705ac6a4a100d8e9c231ce5e270
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-01-31 19:38:09 -0500

    DEVPROD-14608 Extract upstream project into its own query  (#607)

--- apps/spruce/src/pages/waterfall/testData.ts
+++ apps/spruce/src/pages/waterfall/testData.ts
@@ -18,7 +18,6 @@ export const version: Version = {
     "DEVPROD-11387: Remove CSS grid layout, plus some additional description to demonstrate the overflow capabilities of the component (#397)",
   requester: Requester.Gitter,
   revision: "aec8832bace91f0f3b6d8ad3bb3b27fb4263be83",
-  upstreamProject: null,
   order: 10,
 };
 
@@ -36,7 +35,6 @@ export const versionWithGitTag: Version = {
   message: "parsley/v2.1.64",
   requester: Requester.GitTag,
   revision: "deb77a36604446272d610d267f1cd9f95e4fe8ff",
-  upstreamProject: null,
   order: 9,
 };
 
@@ -54,21 +52,6 @@ export const versionWithUpstreamProject: Version = {
   message: "spruce/v4.1.87",
   requester: Requester.GitTag,
   revision: "130948895a46d4fd04292e7783069918e4e7cd5a",
-  upstreamProject: {
-    owner: "evergreen-ci",
-    project: "evergreen",
-    repo: "evergreen",
-    revision: "abcdefg",
-    task: {
-      execution: 0,
-      id: "678",
-    },
-    triggerID: "12345",
-    triggerType: "task",
-    version: {
-      id: "9876",
-    },
-  },
   order: 8,
 };
 
@@ -83,7 +66,6 @@ export const versionBroken: Version = {
     "DEVPROD-11387: Remove CSS grid layout, plus some additional description to demonstrate the overflow capabilities of the component (#397)",
   requester: Requester.Gitter,
   revision: "aec8832bace91f0f3b6d8ad3bb3b27fb4263be83",
-  upstreamProject: null,
   order: 7,
 };
 
@@ -97,7 +79,6 @@ export const inactiveVersion: Version = {
   message: "Inactive Version by Sophie Stadler",
   requester: Requester.Gitter,
   revision: "a659b9908f6be84afd8142e9c2e403783e1385afefaa728792b3c23b9d6acf7a",
-  upstreamProject: null,
   order: 6,
 };
 
@@ -111,7 +92,6 @@ export const inactiveBrokenVersion: Version = {
   message: "Inactive Version by Sophie Stadler",
   requester: Requester.Gitter,
   revision: "a659b9908f6be84afd8142e9c2e403783e1385afefaa728792b3c23b9d6acf7a",
-  upstreamProject: null,
   order: 5,
 };
 
