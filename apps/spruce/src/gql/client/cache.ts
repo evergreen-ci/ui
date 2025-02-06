0032c85f8335be58e0fa4a174d27f73687406d4e
Sophie Stadler <sophie.stadler@mongodb.com>
2025-02-06 15:21:41 -0500

    Add unit tests

--- apps/spruce/src/gql/client/cache.ts
+++ apps/spruce/src/gql/client/cache.ts
@@ -1,6 +1,6 @@
 import { InMemoryCache } from "@apollo/client";
 import { IMAGE_EVENT_LIMIT } from "pages/image/tabs/EventLogTab/useImageEvents";
-import { WaterfallQuery } from "../generated/types";
+import { mergeVersions, readVersions } from "pages/waterfall/caching";
 
 export const cache = new InMemoryCache({
   typePolicies: {
@@ -20,140 +20,11 @@ export const cache = new InMemoryCache({
         },
         waterfall: {
           keyArgs: ["options", ["projectIdentifier"]],
-          read(existing, { args, readField, variables }) {
-            if (!existing) {
-              return undefined;
-            }
-
-            const minOrder = args?.options?.minOrder ?? 0;
-            const maxOrder = args?.options?.maxOrder ?? 0;
-            const limit = variables?.options?.limit ?? 0;
-
-            const existingVersions =
-              readField<WaterfallQuery["waterfall"]["flattenedVersions"]>(
-                "flattenedVersions",
-                existing,
-              ) ?? [];
-
-            const idx = existingVersions.findIndex((v) => {
-              const versionOrder = readField<number>("order", v) ?? 0;
-              if (minOrder) {
-                return versionOrder - 1 === minOrder;
-              }
-              if (maxOrder) {
-                return versionOrder + 1 === maxOrder;
-              }
-              return false;
-            });
-
-            if (idx === -1) {
-              return undefined;
-            }
-
-            let startIndex = maxOrder ? idx : 0;
-            let endIndex = maxOrder ? existingVersions.length : idx;
-            let numActivated = 0;
-
-            // Count backwards for paginating backwards.
-            if (minOrder) {
-              for (let i = endIndex; i >= 0; i--) {
-                if (readField<boolean>("activated", existingVersions[i])) {
-                  numActivated += 1;
-                  if (numActivated === limit) {
-                    startIndex = i;
-                    if (i > 0) {
-                      // Check for leading inactive versions
-                      if (
-                        [...Array(i).keys()].every(
-                          (index) =>
-                            readField<boolean>(
-                              "activated",
-                              existingVersions[index],
-                            ) === false,
-                        )
-                      ) {
-                        startIndex = 0;
-                      }
-                    }
-                    break;
-                  }
-                }
-              }
-            }
-
-            // Count forwards for paginating forwards.
-            if (maxOrder) {
-              for (let i = startIndex; i < existingVersions.length; i++) {
-                if (readField<boolean>("activated", existingVersions[i])) {
-                  numActivated += 1;
-                  if (numActivated === limit) {
-                    endIndex = i;
-                    break;
-                  }
-                }
-              }
-            }
-
-            // Add 1 because slice is [inclusive, exclusive).
-            const flattenedVersions = existingVersions.slice(
-              startIndex,
-              endIndex + 1,
-            );
-
-            const prevOrderNumber =
-              startIndex === 0
-                ? 0
-                : (readField<number>("order", flattenedVersions[0]) ?? 0);
-            const lastVersionOrder = readField<number>(
-              "order",
-              flattenedVersions[flattenedVersions.length - 1],
-            );
-            const nextOrderNumber =
-              !lastVersionOrder || lastVersionOrder === 1
-                ? 0
-                : lastVersionOrder;
-
-            return {
-              flattenedVersions,
-              pagination: {
-                prevPageOrder: prevOrderNumber,
-                nextPageOrder: nextOrderNumber,
-                hasNextPage: nextOrderNumber > 0,
-                hasPrevPage: prevOrderNumber > 0,
-              },
-            };
+          read(...args) {
+            return readVersions(...args);
           },
-          merge(existing, incoming, { readField }) {
-            const existingVersions =
-              readField<WaterfallQuery["waterfall"]["flattenedVersions"]>(
-                "flattenedVersions",
-                existing,
-              ) ?? [];
-            const incomingVersions =
-              readField<WaterfallQuery["waterfall"]["flattenedVersions"]>(
-                "flattenedVersions",
-                incoming,
-              ) ?? [];
-            const versions = [...existingVersions, ...incomingVersions];
-
-            // Use a map to enforce that there are no duplicates.
-            const versionsMap = new Map();
-            versions.forEach((v) => {
-              const order = readField<number>("order", v) ?? 0;
-              versionsMap.set(order, v);
-            });
-
-            const v = Array.from(versionsMap.values()).sort((a, b) => {
-              const aOrder = readField<number>("order", a) ?? 0;
-              const bOrder = readField<number>("order", b) ?? 0;
-              return bOrder - aOrder;
-            });
-
-            const pagination = readField("pagination", incoming);
-            return {
-              flattenedVersions: v,
-              pagination,
-            };
+          merge(...args) {
+            return mergeVersions(...args);
           },
         },
       },
--- apps/spruce/src/pages/waterfall/utils.test.ts
+++ apps/spruce/src/pages/waterfall/utils.test.ts
@@ -1,3 +1,5 @@
+import { FieldFunctionOptions } from "@apollo/client";
+import { mergeVersions, readVersions } from "./caching";
 import { buildVariants } from "./testData";
 import { Version } from "./types";
 import { groupBuildVariants, groupInactiveVersions } from "./utils";
@@ -134,6 +136,255 @@ describe("groupBuildVariants", () => {
         ],
       },
     ]);
+  });
+});
+
+describe("caching functions", () => {
+  describe("mergeVersions", () => {
+    it("merges version arrays", () => {
+      const pagination = {
+        nextPageOrder: 0,
+        prevPageOrder: 0,
+        hasNextPage: true,
+        hasPrevPage: true,
+      };
+      expect(
+        mergeVersions(
+          {
+            flattenedVersions: versions.slice(0, 2),
+          },
+          {
+            flattenedVersions: versions.slice(2, -1),
+            pagination,
+          },
+          {
+            // @ts-expect-error
+            readField: (field, obj) => obj[field],
+          } as FieldFunctionOptions,
+        ),
+      ).toStrictEqual({ flattenedVersions: versions.slice(0, -1), pagination });
+    });
+
+    it("merges version when incoming is newer than existing", () => {
+      const pagination = {
+        nextPageOrder: 3,
+        prevPageOrder: 0,
+        hasNextPage: true,
+        hasPrevPage: false,
+      };
+      expect(
+        mergeVersions(
+          {
+            flattenedVersions: versions.slice(2, -1),
+          },
+          {
+            flattenedVersions: versions.slice(0, 2),
+            pagination,
+          },
+          {
+            // @ts-expect-error
+            readField: (field, obj) => obj[field],
+          } as FieldFunctionOptions,
+        ),
+      ).toStrictEqual({ flattenedVersions: versions.slice(0, -1), pagination });
+    });
+
+    it("deduplicates versions when merging", () => {
+      const pagination = {
+        nextPageOrder: 0,
+        prevPageOrder: 1,
+        hasNextPage: false,
+        hasPrevPage: true,
+      };
+      expect(
+        mergeVersions(
+          {
+            flattenedVersions: versions.slice(0, 4),
+          },
+          {
+            flattenedVersions: versions.slice(2),
+            pagination,
+          },
+          {
+            // @ts-expect-error
+            readField: (field, obj) => obj[field],
+          } as FieldFunctionOptions,
+        ),
+      ).toStrictEqual({ flattenedVersions: versions, pagination });
+    });
+
+    it("returns an identical cache when duplicate data is incoming", () => {
+      const pagination = {
+        nextPageOrder: 0,
+        prevPageOrder: 0,
+        hasNextPage: false,
+        hasPrevPage: false,
+      };
+      expect(
+        mergeVersions(
+          {
+            flattenedVersions: versions,
+          },
+          {
+            flattenedVersions: versions,
+            pagination,
+          },
+          {
+            // @ts-expect-error
+            readField: (field, obj) => obj[field],
+          } as FieldFunctionOptions,
+        ),
+      ).toStrictEqual({ flattenedVersions: versions, pagination });
+    });
+  });
+
+  describe("readVersions", () => {
+    it("returns undefined when the cache is empty", () => {
+      expect(
+        readVersions(
+          undefined,
+          // @ts-expect-error
+          {
+            args: {
+              options: { limit: 5 },
+            },
+            // @ts-expect-error
+            readField: (field, obj) => obj[field],
+          } as FieldFunctionOptions,
+        ),
+      ).toBe(undefined);
+    });
+
+    it("reads the first page and returns limit active versions", () => {
+      expect(
+        readVersions(
+          {
+            flattenedVersions: versions,
+          },
+          // @ts-expect-error
+          {
+            args: {
+              options: { limit: 5, maxOrder: 6 },
+            },
+            // @ts-expect-error
+            readField: (field, obj) => obj[field],
+          } as FieldFunctionOptions,
+        ),
+      ).toStrictEqual({
+        flattenedVersions: versions,
+        pagination: {
+          hasPrevPage: false,
+          hasNextPage: false,
+          prevPageOrder: 0,
+          nextPageOrder: 0,
+        },
+      });
+    });
+
+    it("truncates after limit active versions", () => {
+      expect(
+        readVersions(
+          {
+            flattenedVersions: versions,
+          },
+          // @ts-expect-error
+          {
+            args: {
+              options: { limit: 2, maxOrder: 5 },
+            },
+            // @ts-expect-error
+            readField: (field, obj) => obj[field],
+          } as FieldFunctionOptions,
+        ),
+      ).toStrictEqual({
+        flattenedVersions: versions.slice(1, 3),
+        pagination: {
+          hasPrevPage: true,
+          hasNextPage: true,
+          prevPageOrder: 4,
+          nextPageOrder: 3,
+        },
+      });
+    });
+
+    it("correctly reads a page", () => {
+      expect(
+        readVersions(
+          {
+            flattenedVersions: versions,
+          },
+          // @ts-expect-error
+          {
+            args: {
+              options: {
+                limit: 5,
+                maxOrder: 4,
+              },
+            },
+            // @ts-expect-error
+            readField: (field, obj) => obj[field],
+          } as FieldFunctionOptions,
+        ),
+      ).toStrictEqual({
+        flattenedVersions: versions.slice(2),
+        pagination: {
+          hasPrevPage: true,
+          hasNextPage: false,
+          prevPageOrder: 3,
+          nextPageOrder: 0,
+        },
+      });
+    });
+
+    it("reads a page using minOrder", () => {
+      expect(
+        readVersions(
+          {
+            flattenedVersions: versions,
+          },
+          // @ts-expect-error
+          {
+            args: {
+              options: {
+                limit: 5,
+                minOrder: 2,
+              },
+            },
+            // @ts-expect-error
+            readField: (field, obj) => obj[field],
+          } as FieldFunctionOptions,
+        ),
+      ).toStrictEqual({
+        flattenedVersions: versions.slice(0, 3),
+        pagination: {
+          hasPrevPage: false,
+          hasNextPage: true,
+          prevPageOrder: 0,
+          nextPageOrder: 3,
+        },
+      });
+    });
+
+    it("returns undefined when the order is not found in the cache", () => {
+      expect(
+        readVersions(
+          {
+            flattenedVersions: versions,
+          },
+          // @ts-expect-error
+          {
+            args: {
+              options: {
+                limit: 5,
+                minOrder: 8,
+              },
+            },
+            // @ts-expect-error
+            readField: (field, obj) => obj[field],
+          } as FieldFunctionOptions,
+        ),
+      ).toBe(undefined);
+    });
   });
 });
 
