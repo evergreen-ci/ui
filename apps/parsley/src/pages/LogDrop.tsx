177da4bd699bf77ad463709626c6b2d9f3ec42bf
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-02-12 11:31:56 -0500

    Set page title for task logs

--- apps/parsley/src/pages/LogDrop.tsx
+++ apps/parsley/src/pages/LogDrop.tsx
@@ -1,10 +1,11 @@
+import { usePageTitle } from "@evg-ui/lib/hooks/usePageTitle";
 import LogWindow from "components/LogWindow";
 import { useLogContext } from "context/LogContext";
 import FileDropper from "./LogDrop/FileDropper";
 
 const LogDrop = () => {
-  const { hasLogs } = useLogContext();
-
+  const { hasLogs, logMetadata } = useLogContext();
+  usePageTitle(`Parsley | ${hasLogs ? logMetadata?.fileName : "Upload logs"}`);
   return hasLogs ? <LogWindow /> : <FileDropper />;
 };
 
