3e3b8e2a4133c3f59109c891f3bbf73723225ebd
minnakt <47064971+minnakt@users.noreply.github.com>
2025-02-20 10:32:32 -0500

    DEVPROD-8866: Upgrade to ESLint v9 (#632)

---
+++ apps/parsley/eslint.config.js
@@ -0,0 +1,33 @@
+import { fixupPluginRules } from "@eslint/compat";
+import * as sortKeysPlugin from "eslint-plugin-sort-keys-plus";
+import tseslint from "typescript-eslint";
+import baseConfig, { ERROR, errorIfStrict } from "@evg-ui/eslint-config";
+
+export default tseslint.config(
+  ...baseConfig,
+  // ESLint (@eslint/js) overrides for Parsley.
+  {
+    name: "@eslint/js/parsley-overrides",
+    files: ["src/**/*.ts?(x)"],
+    rules: {
+      "sort-imports": [ERROR, { ignoreDeclarationSort: true }],
+    },
+  },
+  // Sort Keys File ESLint (eslint-plugin-sort-keys-plus) settings.
+  // TODO DEVPROD-6890: This plugin should be dropped because the project has been abandoned.
+  // It may potentially be replaced by Perfectionist's sort-objects rule (https://perfectionist.dev/rules/sort-objects).
+  {
+    name: "sort-keys-plus/rules",
+    files: ["src/**/*.ts?(x)"],
+    plugins: {
+      "sort-keys-plus": fixupPluginRules(sortKeysPlugin),
+    },
+    rules: {
+      "sort-keys-plus/sort-keys": [
+        errorIfStrict,
+        "asc",
+        { allowLineSeparatedGroups: true, natural: true },
+      ],
+    },
+  },
+);
