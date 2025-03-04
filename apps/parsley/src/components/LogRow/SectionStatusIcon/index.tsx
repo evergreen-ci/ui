9a5318cf9a98d26adb9b9ca48f19d04fe09f0597
minnakt <minna.kt@mongodb.com>
2025-03-04 10:33:59 -0500

    Update Parsley imports

--- apps/parsley/src/components/LogRow/SectionStatusIcon/index.tsx
+++ apps/parsley/src/components/LogRow/SectionStatusIcon/index.tsx
@@ -1,5 +1,5 @@
 import { palette } from "@leafygreen-ui/palette";
-import Icon from "components/Icon";
+import Icon from "@evg-ui/lib/components/Icon";
 import { SectionStatus } from "constants/logs";
 
 const { gray, red } = palette;
