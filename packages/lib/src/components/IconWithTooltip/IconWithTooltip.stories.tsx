6ff2013e585d11b5dff8ed0799cf14dd8d181f10
minnakt <minna.kt@mongodb.com>
2025-03-04 10:14:40 -0500

    Move IconWithTooltip component

---
+++ packages/lib/src/components/IconWithTooltip/IconWithTooltip.stories.tsx
@@ -0,0 +1,67 @@
+import styled from "@emotion/styled";
+import { palette } from "@leafygreen-ui/palette";
+import { Size, glyphs } from "components/Icon";
+import { size } from "constants/tokens";
+import { CustomMeta, CustomStoryObj } from "test_utils/types";
+import IconWithTooltip from ".";
+
+const { green } = palette;
+
+const Sizes = {
+  [Size.Small]: 14,
+  [Size.Default]: 16,
+  [Size.Large]: 20,
+  [Size.XLarge]: 24,
+};
+
+export default {
+  component: IconWithTooltip,
+} satisfies CustomMeta<typeof IconWithTooltip>;
+
+export const Default: CustomStoryObj<typeof IconWithTooltip> = {
+  argTypes: {
+    color: {
+      control: "color",
+    },
+    size: {
+      control: { type: "select" },
+      options: Object.values(Sizes),
+    },
+  },
+  args: {
+    color: green.dark3,
+    size: Sizes[Size.Default],
+    children: "Tooltip Text",
+  },
+  render: ({ children, ...rest }) => (
+    <Container>
+      {Object.keys(glyphs).map((name) => (
+        <IconContainer key={name}>
+          <IconWithTooltip {...rest} glyph={name}>
+            {children}
+          </IconWithTooltip>
+          <span>{name}</span>
+        </IconContainer>
+      ))}
+    </Container>
+  ),
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const IconContainer = styled.div`
+  width: 150px;
+  height: 70px;
+  flex-shrink: 0;
+  text-align: center;
+  border: 1px solid #babdbe;
+  border-radius: ${size.xxs};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin: 0.5rem;
+`;
