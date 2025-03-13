4db2fb356ae40a9e3138585249a8643f4f40d251
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-03-13 14:17:30 -0400

    Move global providers so they correctly wrap the app and expose login page

--- apps/parsley/src/App.tsx
+++ apps/parsley/src/App.tsx
@@ -1,28 +1,47 @@
 import styled from "@emotion/styled";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { AuthProvider } from "@evg-ui/lib/context/Auth";
 import { ErrorBoundary } from "components/ErrorHandling";
 import { GlobalStyles } from "components/styles";
 import routes from "constants/routes";
 import { GlobalProviders } from "context";
 import Content from "pages";
 import { Login } from "pages/Login";
-import { isDevelopmentBuild } from "utils/environmentVariables";
+import {
+  evergreenURL,
+  getCorpLoginURL,
+  isDevelopmentBuild,
+  isRemoteEnv,
+  parsleyURL,
+} from "utils/environmentVariables";
 
 const App = () => (
   <ErrorBoundary>
     <GlobalStyles />
-    <Router>
-      <GlobalProviders>
+    <AuthProvider
+      localAppURL={evergreenURL || ""}
+      localAuthURL={`${parsleyURL}/login`}
+      remoteAuthURL={getCorpLoginURL()}
+      shouldUseLocalAuth={!isRemoteEnv()}
+    >
+      <Router>
         <AppWrapper>
           <Routes>
             {isDevelopmentBuild() && (
               <Route element={<Login />} path={routes.login} />
             )}
-            <Route element={<Content />} path="/*" />
+            <Route
+              element={
+                <GlobalProviders>
+                  <Content />
+                </GlobalProviders>
+              }
+              path="/*"
+            />
           </Routes>
         </AppWrapper>
-      </GlobalProviders>
-    </Router>
+      </Router>
+    </AuthProvider>
   </ErrorBoundary>
 );
 
