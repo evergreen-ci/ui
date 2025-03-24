72e28e0545583f9a70cd8ded387e2385d483297f
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-03-24 15:47:09 -0400

    Move omit into object file

--- apps/spruce/src/utils/object/index.ts
+++ apps/spruce/src/utils/object/index.ts
@@ -1,3 +1,15 @@
-import { omit } from "./omit";
-
-export { omit };
+/**
+ * `omit` is a utility function that removes the supplied key(s) from an object.
+ * @param obj - The object to process.
+ * @param params - The keys to omit.
+ * @returns - A new object without the supplied key(s).
+ * @example omit({ a: 1, b: 1, c: 1 }, ["b", "c"]) => { a: 1 }
+ */
+export const omit = <T extends object, K extends [...(keyof T)[]]>(
+  obj: T,
+  params: K,
+) => {
+  const newObj = { ...obj };
+  params.forEach((param) => delete newObj[param]);
+  return newObj as Omit<T, K[number]>;
+};
--- apps/spruce/src/utils/object/omit.ts
+++
@@ -1,8 +0,0 @@
-export const omit = <T extends object, K extends [...(keyof T)[]]>(
-  obj: T,
-  params: K,
-) => {
-  const newObj = { ...obj };
-  params.forEach((param) => delete newObj[param]);
-  return newObj as Omit<T, K[number]>;
-};
