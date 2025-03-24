ba0b94cc7ff00204781e22fa00771821586b8d74
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-03-24 10:20:18 -0400

    Remove unnecessary throw error handling

--- packages/deploy-utils/src/build-and-push/index.ts
+++ packages/deploy-utils/src/build-and-push/index.ts
@@ -8,12 +8,8 @@ import { pushToS3 } from "../utils/s3";
  * @param bucket - bucket to push to
  */
 export const buildAndPush = (bucket: string) => {
-  try {
-    // Disable script mode to let vite build the project
-    execSync("VITE_SCRIPT_MODE=0 yarn build", { stdio: "inherit" });
-  } catch (e) {
-    throw new Error("Deployment to S3 failed", { cause: e });
-  }
+  // Disable script mode to let vite build the project
+  execSync("VITE_SCRIPT_MODE=0 yarn build", { stdio: "inherit" });
 
   const currentCommit = getCurrentCommit();
   writeFileSync("dist/commit.txt", currentCommit);
