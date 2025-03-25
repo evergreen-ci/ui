1f9f69b98e54c79eb5d7d1af07af94397df87955
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-03-24 20:49:42 -0400

    Date primitives as JSON values

--- apps/spruce/src/utils/object/types.ts
+++ apps/spruce/src/utils/object/types.ts
@@ -1,6 +1,6 @@
 type Primitive = string | number | boolean | null | undefined;
 type JSONObject = { [key: string]: JSONValue };
 type JSONArray = JSONValue[];
-type JSONValue = Primitive | JSONArray | JSONObject;
+type JSONValue = Primitive | JSONArray | JSONObject | Date;
 
 export type { JSONValue, JSONObject, JSONArray, Primitive };
