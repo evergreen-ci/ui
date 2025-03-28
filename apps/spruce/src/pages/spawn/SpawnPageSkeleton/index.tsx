25845343e46c5b2a00a3416312035be95aaf41c2
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-03-28 16:20:38 -0400

    SpawnPageSkeleton

---
+++ apps/spruce/src/pages/spawn/SpawnPageSkeleton/index.tsx
@@ -0,0 +1,23 @@
+import styled from "@emotion/styled";
+import { Skeleton, TableSkeleton } from "@leafygreen-ui/skeleton-loader";
+import { size } from "@evg-ui/lib/constants/tokens";
+
+const SpawnPageSkeleton = () => (
+  <div>
+    <HeaderSkeleton />
+    <ButtonSkeleton />
+    <StyledTableSkeleton />
+  </div>
+);
+export default SpawnPageSkeleton;
+
+const StyledTableSkeleton = styled(TableSkeleton)`
+  margin-top: 20px;
+`;
+const HeaderSkeleton = styled(Skeleton)`
+  width: 400px;
+`;
+const ButtonSkeleton = styled(Skeleton)`
+  margin-top: ${size.l};
+  width: 200px;
+`;
