4b5741ebbd0ba889fcb27f1f67ea50dd7e8e4f3b
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-04-22 03:27:16 -0400

    Remove unused param

--- apps/spruce/src/pages/projectSettings/tabs/ViewsAndFiltersTab/transformers.test.ts
+++ apps/spruce/src/pages/projectSettings/tabs/ViewsAndFiltersTab/transformers.test.ts
@@ -1,6 +1,5 @@
 import { ProjectSettingsInput, RepoSettingsInput } from "gql/generated/types";
 import { data } from "../testData";
-import { ProjectType } from "../utils";
 import { formToGql, gqlToForm } from "./transformers";
 import { ViewsFormState } from "./types";
 
@@ -8,9 +7,7 @@ const { projectBase, repoBase } = data;
 
 describe("repo data", () => {
   it("correctly converts from GQL to a form", () => {
-    expect(
-      gqlToForm(repoBase, { projectType: ProjectType.Repo }),
-    ).toStrictEqual(repoForm);
+    expect(gqlToForm(repoBase)).toStrictEqual(repoForm);
   });
 
   it("correctly converts from a form to GQL", () => {
@@ -20,9 +17,7 @@ describe("repo data", () => {
 
 describe("project data", () => {
   it("correctly converts from GQL to a form", () => {
-    expect(
-      gqlToForm(projectBase, { projectType: ProjectType.Project }),
-    ).toStrictEqual(projectForm);
+    expect(gqlToForm(projectBase)).toStrictEqual(projectForm);
   });
 
   it("correctly converts from a form to GQL", () => {
