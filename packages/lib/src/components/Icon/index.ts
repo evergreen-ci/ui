ec0c49f7798a2b703975329d2e8d72c7ab339673
minnakt <minna.kt@mongodb.com>
2025-06-03 11:57:35 -0400

    DEVPROD-18077: Update Evergreen logo for Pride Month

--- packages/lib/src/components/Icon/index.ts
+++ packages/lib/src/components/Icon/index.ts
@@ -1,6 +1,7 @@
 import { Size, createIconComponent, glyphs } from "@leafygreen-ui/icon";
 import AnimatedIcon from "./AnimatedIcon";
 import * as icons from "./icons";
+import { PrideLogo } from "./icons/logos/PrideLogo";
 
 const glyphMap = {
   ...glyphs,
@@ -9,4 +10,4 @@ const glyphMap = {
 
 export { glyphMap as glyphs, Size };
 export default createIconComponent(glyphMap);
-export { AnimatedIcon };
+export { AnimatedIcon, PrideLogo };
