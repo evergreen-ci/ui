3c7ae70931f2846ec18c21e2e0f1cc508fe97748
minnakt <47064971+minnakt@users.noreply.github.com>
2025-06-09 11:18:50 -0400

    DEVPROD-9457: Gray out restart button for merge queue tasks (#833)

--- apps/spruce/src/components/PatchActionButtons/RestartPatch.tsx
+++ apps/spruce/src/components/PatchActionButtons/RestartPatch.tsx
@@ -1,18 +1,23 @@
 import { useState } from "react";
-import Button from "@leafygreen-ui/button";
+import Button, { Size as ButtonSize } from "@leafygreen-ui/button";
+import Tooltip, { Align, Justify } from "@leafygreen-ui/tooltip";
+import { zIndex } from "@evg-ui/lib/constants/tokens";
 import { DropdownItem } from "components/ButtonDropdown";
 import { VersionRestartModal } from "components/VersionRestartModal";
 
 interface RestartPatchProps {
-  patchId: string;
   disabled?: boolean;
   isButton?: boolean;
+  isMergeQueuePatch: boolean;
+  patchId: string;
   refetchQueries: string[];
   visibilityControl?: [boolean, React.Dispatch<React.SetStateAction<boolean>>];
 }
+
 export const RestartPatch: React.FC<RestartPatchProps> = ({
   disabled = false,
   isButton,
+  isMergeQueuePatch,
   patchId,
   refetchQueries,
   visibilityControl,
@@ -25,31 +30,45 @@ export const RestartPatch: React.FC<RestartPatchProps> = ({
 
   const onClick = () => setIsVisible(!isVisible);
 
+  const message = isMergeQueuePatch
+    ? "GitHub merge queue patches cannot be restarted."
+    : "This patch cannot be restarted.";
+
   return (
     <>
-      {isButton ? (
-        <Button
-          data-cy="restart-version"
-          disabled={disabled}
-          onClick={onClick}
-          size="small"
-        >
-          Restart
-        </Button>
-      ) : (
-        <DropdownItem
-          data-cy="restart-version"
-          disabled={disabled}
-          onClick={onClick}
-        >
-          Restart
-        </DropdownItem>
-      )}
+      <Tooltip
+        align={isButton ? Align.Top : Align.Left}
+        enabled={disabled}
+        justify={Justify.End}
+        popoverZIndex={zIndex.tooltip}
+        trigger={
+          isButton ? (
+            <Button
+              data-cy="restart-version"
+              disabled={disabled}
+              onClick={onClick}
+              size={ButtonSize.Small}
+            >
+              Restart
+            </Button>
+          ) : (
+            <span>
+              <DropdownItem
+                data-cy="restart-version"
+                disabled={disabled}
+                onClick={onClick}
+              >
+                Restart
+              </DropdownItem>
+            </span>
+          )
+        }
+      >
+        {message}
+      </Tooltip>
       <VersionRestartModal
         onCancel={() => setIsVisible(false)}
-        onOk={() => {
-          setIsVisible(false);
-        }}
+        onOk={() => setIsVisible(false)}
         refetchQueries={refetchQueries}
         versionId={patchId}
         visible={isVisible}
