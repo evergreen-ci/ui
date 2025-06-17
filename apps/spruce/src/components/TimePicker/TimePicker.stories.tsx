c9f762ea944e315f40effb0b910fe3d363a07812
minnakt <minna.kt@mongodb.com>
2025-06-17 13:04:16 -0400

    Create `TimePicker` story

---
+++ apps/spruce/src/components/TimePicker/TimePicker.stories.tsx
@@ -0,0 +1,31 @@
+import { CustomStoryObj, CustomMeta } from "@evg-ui/lib/test_utils/types";
+
+import TimePicker from ".";
+
+export default {
+  component: TimePicker,
+} satisfies CustomMeta<typeof TimePicker>;
+
+export const Default: CustomStoryObj<typeof TimePicker> = {
+  render: (args) => (
+    <div style={{ width: "110px" }}>
+      <TimePicker
+        {...args}
+        onDateChange={() => console.log("date changed")}
+        value={new Date(args.value)}
+      />
+    </div>
+  ),
+  args: {
+    disabled: false,
+    value: new Date(),
+  },
+  argTypes: {
+    disabled: {
+      control: { type: "boolean" },
+    },
+    value: {
+      control: { type: "date" },
+    },
+  },
+};
