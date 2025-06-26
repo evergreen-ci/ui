fd3e9353f1525f958bb791f0095d089076cc994f
minnakt <47064971+minnakt@users.noreply.github.com>
2025-06-26 11:03:49 -0400

    DEVPROD-8062: Upgrade `@emotion` to v11.14.0 (#882)

---
+++ packages/lib/src/test_utils/snapshotSerializer.ts
@@ -0,0 +1,18 @@
+import { createSerializer } from "@emotion/jest";
+
+const serializer = createSerializer({
+  classNameReplacer(className, index) {
+    if (className.startsWith("css")) {
+      const hashEnd = className.indexOf("-", "css".length + 1);
+      const newClassName =
+        hashEnd > 0
+          ? `emotion${className.substring(hashEnd)}`
+          : `emotion-${index}`;
+      return newClassName.trim();
+    }
+    return className;
+  },
+  includeStyles: false,
+});
+
+export default serializer;
