8fa26e0572cf4f5f1b59bf076455a7de6ddfc219
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-07-09 14:52:36 -0400

    Fix test data

--- apps/spruce/src/pages/version/BuildVariantCard/testData.ts
+++ apps/spruce/src/pages/version/BuildVariantCard/testData.ts
@@ -12,7 +12,7 @@ const mocks: ApolloMock<
   {
     request: {
       query: BUILD_VARIANTS_STATS,
-      variables: { id: "version", includeNeverActivatedTasks: false },
+      variables: { id: "version", includeNeverActivatedTasks: undefined },
     },
     result: {
       data: {
