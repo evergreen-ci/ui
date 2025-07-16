ceb39b7bd6982d57bdb61e6a1d056153459c6edb
Annie Black <annie.black@mongodb.com>
2025-07-16 17:14:10 -0400

    DEVPROD-17219 remove spot fleet options from distro UI (#937)

--- apps/spruce/src/pages/distroSettings/tabs/ProviderTab/styles.ts
+++ apps/spruce/src/pages/distroSettings/tabs/ProviderTab/styles.ts
@@ -1,7 +1,6 @@
 import { css } from "@emotion/react";
 import { fontFamilies } from "@leafygreen-ui/tokens";
 import { size } from "@evg-ui/lib/constants/tokens";
-import { STANDARD_FIELD_WIDTH } from "components/SpruceForm/utils";
 
 const textAreaCSS = css`
   box-sizing: border-box;
@@ -16,13 +15,9 @@ const mergeCheckboxCSS = css`
   margin-bottom: -20px;
 `;
 
-const capacityCheckboxCSS = css`
-  max-width: ${STANDARD_FIELD_WIDTH}px;
-`;
-
 const indentCSS = css`
   box-sizing: border-box;
   padding-left: ${size.m};
 `;
 
-export { textAreaCSS, mergeCheckboxCSS, capacityCheckboxCSS, indentCSS };
+export { textAreaCSS, mergeCheckboxCSS, indentCSS };
