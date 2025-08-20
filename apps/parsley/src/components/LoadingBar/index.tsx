67ed6ee19a28e10ded56843814514f4fcb3c18cb
minnakt <minna.kt@mongodb.com>
2025-08-20 11:12:58 -0400

    Change loading bar color

--- apps/parsley/src/components/LoadingBar/index.tsx
+++ apps/parsley/src/components/LoadingBar/index.tsx
@@ -40,7 +40,7 @@ const Bar = styled.div<{ progress: number; indeterminate: boolean }>`
     border-bottom-right-radius: ${size.xxs};
     `}
   height: 6px;
-  background-color: ${green.base};
+  background-color: ${green.dark1};
   ${({ indeterminate, progress }) =>
     indeterminate
       ? indeterminateAnimation
