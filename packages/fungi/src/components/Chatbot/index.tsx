25c1605be17b77cb5df33451b1541435e1d3f537
Sophie Stadler <sophie.stadler@mongodb.com>
2025-08-21 15:22:23 -0400

    Add assistantName prop

--- packages/fungi/src/components/Chatbot/index.tsx
+++ packages/fungi/src/components/Chatbot/index.tsx
@@ -1,21 +1,34 @@
 import { useChat } from "@ai-sdk/react";
 import { ChatWindow } from "@lg-chat/chat-window";
 import { InputBar } from "@lg-chat/input-bar";
+import {
+  LeafyGreenChatProvider,
+  LeafyGreenChatProviderProps,
+} from "@lg-chat/leafygreen-chat-provider";
 import { Message } from "@lg-chat/message";
 import { MessageFeed } from "@lg-chat/message-feed";
 import { DefaultChatTransport } from "ai";
 
 type ChatbotProps = {
   apiUrl: string;
-};
+  bodyData?: object;
+} & Pick<LeafyGreenChatProviderProps, "assistantName">;
 
-export const Chatbot = ({ apiUrl }: React.PropsWithChildren<ChatbotProps>) => {
+export const Chatbot = ({
+  apiUrl,
+  assistantName,
+  bodyData,
+}: React.PropsWithChildren<ChatbotProps>) => {
   const { messages, sendMessage } = useChat({
     transport: new DefaultChatTransport({
       api: apiUrl,
       prepareSendMessagesRequest({ id, messages: outgoingMessages }) {
         return {
-          body: { message: outgoingMessages[outgoingMessages.length - 1], id },
+          body: {
+            ...(bodyData ?? {}),
+            message: outgoingMessages[outgoingMessages.length - 1],
+            id,
+          },
         };
       },
     }),
@@ -26,20 +39,22 @@ export const Chatbot = ({ apiUrl }: React.PropsWithChildren<ChatbotProps>) => {
   };
 
   return (
-    <ChatWindow title="">
-      {/* ChatWindow includes a ChatProvider */}
-      <MessageFeed>
-        {messages.map(({ id, parts, role }) => (
-          <Message key={id} isSender={role === "user"}>
-            {parts.map((part, index) =>
-              part.type === "text" ? (
-                <span key={index}>{part.text}</span>
-              ) : null,
-            )}
-          </Message>
-        ))}
-      </MessageFeed>
-      <InputBar onMessageSend={handleSend} />
-    </ChatWindow>
+    <LeafyGreenChatProvider assistantName={assistantName}>
+      {/* This title won't be visible since we're using the compact variant */}
+      <ChatWindow title="">
+        <MessageFeed>
+          {messages.map(({ id, parts, role }) => (
+            <Message key={id} isSender={role === "user"}>
+              {parts.map((part, index) =>
+                part.type === "text" ? (
+                  <span key={index}>{part.text}</span>
+                ) : null,
+              )}
+            </Message>
+          ))}
+        </MessageFeed>
+        <InputBar onMessageSend={handleSend} />
+      </ChatWindow>
+    </LeafyGreenChatProvider>
   );
 };
