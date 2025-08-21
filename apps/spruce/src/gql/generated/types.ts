a93458ec73cc4a0ab8f9d5ca0e960db56a1a43e4
bynn <bynn.lee@mongodb.com>
2025-08-21 16:17:08 -0400

    codegen

--- apps/spruce/src/gql/generated/types.ts
+++ apps/spruce/src/gql/generated/types.ts
@@ -3601,6 +3601,7 @@ export type SelectorInput = {
 
 export type ServiceFlags = {
   __typename?: "ServiceFlags";
+  adminParameterStoreDisabled: Scalars["Boolean"]["output"];
   agentStartDisabled: Scalars["Boolean"]["output"];
   alertsDisabled: Scalars["Boolean"]["output"];
   backgroundReauthDisabled: Scalars["Boolean"]["output"];
@@ -3639,6 +3640,7 @@ export type ServiceFlags = {
 };
 
 export type ServiceFlagsInput = {
+  adminParameterStoreDisabled: Scalars["Boolean"]["input"];
   agentStartDisabled: Scalars["Boolean"]["input"];
   alertsDisabled: Scalars["Boolean"]["input"];
   backgroundReauthDisabled: Scalars["Boolean"]["input"];
