36f340900a757ba8e7218801663629ae3d282d6c
Sophie Stadler <sophie.stadler@mongodb.com>
2025-08-25 13:53:45 -0400

    Add storybook and unit tests to fungi

--- packages/fungi/vitest.config.ts
+++ packages/fungi/vitest.config.ts
@@ -1,6 +1,24 @@
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@leafygreen-ui/emotion": resolve(
+        __dirname,
+        "./config/leafygreen-ui/emotion",
+      ),
+    },
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"],
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    globalSetup: "./config/vitest/global-setup.ts",
+    outputFile: { junit: "./bin/vitest/junit.xml" },
+    reporters: ["default", ...(process.env.CI === "true" ? ["junit"] : [])],
+    setupFiles: "./config/vitest/setup-tests.ts",
+  },
 });
