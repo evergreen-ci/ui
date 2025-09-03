f46ff8a7d289d8b68fbb819007b6167b515bcd13
Sophie Stadler <sophie.stadler@mongodb.com>
2025-09-03 15:33:23 -0400

    Improve tests

--- packages/fungi/src/ChatFeed/ChatFeed.test.tsx
+++ packages/fungi/src/ChatFeed/ChatFeed.test.tsx
@@ -8,11 +8,14 @@ import { ChatProvider } from "../Context";
 import { ChatFeed } from ".";
 
 describe("ChatFeed", () => {
+  beforeEach(() => {
+    // jsdom doesn't support layouting HTML, so mock this.
+    HTMLDivElement.prototype.scrollTo = () => {};
+  });
+
   it("aligns user messages to the right", async () => {
     const user = userEvent.setup();
 
-    // jsdom doesn't support layouting HTML, so mock this.
-    HTMLDivElement.prototype.scrollTo = () => {};
     render(<ChatFeed apiUrl="/foo" />, {
       wrapper: createWrapper(ChatProvider, { appName: "Parsley AI Test" }),
     });
@@ -22,34 +25,49 @@ describe("ChatFeed", () => {
 
     await user.type(textarea, message);
     await user.click(screen.getByRole("button"));
-    expect(screen.getByText(message)?.parentElement?.parentElement).toHaveStyle(
-      "align-items: flex-end",
-    );
+    expect(
+      screen.queryByText(message)?.parentElement?.parentElement,
+    ).toHaveStyle("align-items: flex-end");
   });
 
-  it("shows suggested prompts on initial render and hides after send", async () => {
-    const user = userEvent.setup();
+  describe("prompt suggestions", () => {
+    it("shows suggested prompts on initial render and hides after sending a custom message", async () => {
+      const user = userEvent.setup();
 
-    // jsdom doesn't support layouting HTML, so mock this.
-    HTMLDivElement.prototype.scrollTo = () => {};
-    render(<ChatFeed apiUrl="/foo" chatSuggestions={["Foo", "Bar"]} />, {
-      wrapper: createWrapper(ChatProvider, { appName: "Parsley AI Test" }),
+      render(<ChatFeed apiUrl="/foo" chatSuggestions={["Foo", "Bar"]} />, {
+        wrapper: createWrapper(ChatProvider, { appName: "Parsley AI Test" }),
+      });
+
+      const message = "Why did my log fail?";
+      const textarea = screen.getByRole("textbox");
+
+      expect(screen.getByText("Foo")).toBeVisible();
+      await user.type(textarea, message);
+      await user.click(screen.getByRole("button", { name: "Send message" }));
+      expect(screen.queryByText("Foo")).not.toBeInTheDocument();
+      expect(screen.queryByText("Suggested Prompts")).not.toBeInTheDocument();
     });
 
-    const message = "Why did my log fail?";
-    const textarea = screen.getByRole("textbox");
+    it("sends a suggested prompt on click", async () => {
+      const user = userEvent.setup();
 
-    expect(screen.getByText("Foo")).toBeVisible();
-    await user.type(textarea, message);
-    await user.click(screen.getByRole("button", { name: "Send message" }));
-    expect(screen.queryByText("Foo")).not.toBeInTheDocument();
+      render(<ChatFeed apiUrl="/foo" chatSuggestions={["Foo", "Bar"]} />, {
+        wrapper: createWrapper(ChatProvider, { appName: "Parsley AI Test" }),
+      });
+
+      expect(screen.getByText("Suggested Prompts")).toBeVisible();
+      expect(screen.getByText("Bar")).toBeVisible();
+      await user.click(screen.getByRole("button", { name: "Bar" }));
+      expect(screen.queryByText("Suggested Prompts")).not.toBeInTheDocument();
+      expect(
+        screen.queryByText("Bar")?.parentElement?.parentElement,
+      ).toHaveStyle("align-items: flex-end");
+    });
   });
 
   it("shows disclaimer on initial render and hides after send", async () => {
     const user = userEvent.setup();
 
-    // jsdom doesn't support layouting HTML, so mock this.
-    HTMLDivElement.prototype.scrollTo = () => {};
     render(<ChatFeed apiUrl="/foo" disclaimerContent="Lorem ipsem" />, {
       wrapper: createWrapper(ChatProvider, { appName: "Parsley AI Test" }),
     });
