982ee5518d3551e2c6ca23571b1d876a534864f4
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-09-10 10:28:54 -0400

    Rename new prop to transformNodes

--- apps/parsley/src/utils/renderHtml/index.tsx
+++ apps/parsley/src/utils/renderHtml/index.tsx
@@ -1,4 +1,5 @@
 import parse, {
+  DOMNode,
   Element,
   HTMLReactParserOptions,
   domToReact,
@@ -7,7 +8,7 @@ import { escapeTags } from "utils/escapeTags";
 
 interface renderHtmlOptions extends HTMLReactParserOptions {
   preserveAttributes?: string[];
-  transform?: {
+  transformNode?: {
     [key: string]: React.ReactNode;
   };
 }
@@ -29,8 +30,8 @@ const renderHtml = (html: string = "", options: renderHtmlOptions = {}) => {
   return parse(escapedHtml, {
     replace: (domNode) => {
       if (domNode instanceof Element) {
-        if (options.transform && options.transform[domNode.name]) {
-          const SwapComponent = options.transform[domNode.name];
+        if (options.transformNode && options.transformNode[domNode.name]) {
+          const SwapComponent = options.transformNode[domNode.name];
           // SwapComponent is just what ever component we want to return from the transform object
           const extraProps =
             options.preserveAttributes &&
@@ -41,7 +42,7 @@ const renderHtml = (html: string = "", options: renderHtmlOptions = {}) => {
           return (
             // @ts-expect-error: Compiler has trouble interpreting SwapComponent as JSX element.
             <SwapComponent className={extraProps.class} {...extraProps}>
-              {domToReact(domNode.children)}
+              {domToReact(domNode.children as DOMNode[])}
             </SwapComponent>
           );
         }
