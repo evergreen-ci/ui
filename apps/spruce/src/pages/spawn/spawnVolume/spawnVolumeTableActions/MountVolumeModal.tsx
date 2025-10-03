72df5cae4d925189ba2a50c38731c31dde59acb4
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-10-03 11:19:02 -0400

    Add links to docs for mounting for volumes

--- apps/spruce/src/pages/spawn/spawnVolume/spawnVolumeTableActions/MountVolumeModal.tsx
+++ apps/spruce/src/pages/spawn/spawnVolume/spawnVolumeTableActions/MountVolumeModal.tsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import { useMutation } from "@apollo/client";
 import styled from "@emotion/styled";
 import { Disclaimer } from "@leafygreen-ui/typography";
+import { StyledLink } from "@evg-ui/lib/components/styles";
 import { size } from "@evg-ui/lib/constants/tokens";
 import { useToastContext } from "@evg-ui/lib/context/toast";
 import { useSpawnAnalytics } from "analytics/spawn/useSpawnAnalytics";
 import { ConfirmationModal } from "components/ConfirmationModal";
 import { ModalContent, MountVolumeSelect } from "components/Spawn";
+import { hostMountVolumeDocumentationUrl } from "constants/externalResources";
 import {
   AttachVolumeToHostMutation,
   AttachVolumeToHostMutationVariables,
@@ -79,6 +81,16 @@ export const MountVolumeModal: React.FC<Props> = ({
           selectedHostId={selectedHostId}
           targetAvailabilityZone={targetAvailabilityZone}
         />
+        This action will only attach the volume to the host. You will need to
+        manually{" "}
+        <StyledLink
+          hideExternalIcon={false}
+          href={hostMountVolumeDocumentationUrl}
+          target="_blank"
+        >
+          mount the volume in the host
+        </StyledLink>
+        .
         <StyledDisclaimer>
           {`Only shows running hosts in zone ${targetAvailabilityZone}.`}
         </StyledDisclaimer>
