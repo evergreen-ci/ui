f02745616ab669f931d9c95a5e9138da72d13795
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-10-07 15:53:16 -0400

    Add regex search on BuildVariantCard

--- apps/spruce/src/pages/configurePatch/configurePatchCore/ConfigureBuildVariants/BuildVariantCard.tsx
+++ apps/spruce/src/pages/configurePatch/configurePatchCore/ConfigureBuildVariants/BuildVariantCard.tsx
@@ -1,8 +1,12 @@
+import { useEffect, useRef, useState } from "react";
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 import Badge, { Variant } from "@leafygreen-ui/badge";
+import IconButton from "@leafygreen-ui/icon-button";
 import { palette } from "@leafygreen-ui/palette";
+import { SearchInput } from "@leafygreen-ui/search-input";
 import { Body, Description } from "@leafygreen-ui/typography";
+import Icon from "@evg-ui/lib/components/Icon";
 import { size } from "@evg-ui/lib/constants/tokens";
 import { SiderCard } from "components/styles";
 import { Divider } from "components/styles/divider";
@@ -25,50 +29,100 @@ const BuildVariantCard: React.FC<BuildVariantCardProps> = ({
   onClick,
   selectedMenuItems,
   title,
-}) => (
-  <StyledSiderCard>
-    <Container>
-      <Body weight="medium">{title}</Body>
-      <Description>
-        Use Shift + Click to edit multiple variants simultaneously.
-      </Description>
-      <Divider />
-    </Container>
-    <ScrollableBuildVariantContainer>
-      {menuItems.map(({ displayName, name, taskCount }) => {
-        const isSelected = selectedMenuItems.includes(name);
-        return (
-          <BuildVariant
-            key={name}
-            data-cy={dataCy}
-            data-selected={isSelected}
-            isSelected={isSelected}
-            onClick={onClick(name)}
-          >
-            <VariantName>
-              <Body weight={isSelected || taskCount > 0 ? "medium" : "regular"}>
-                {displayName}
-              </Body>
-            </VariantName>
-            {taskCount > 0 && (
-              <StyledBadge
-                data-cy="task-count-badge"
-                variant={isSelected ? Variant.DarkGray : Variant.LightGray}
-              >
-                {taskCount}
-              </StyledBadge>
-            )}
-          </BuildVariant>
-        );
-      })}
-    </ScrollableBuildVariantContainer>
-  </StyledSiderCard>
-);
+}) => {
+  const [filteredMenuItems, setFilteredMenuItems] =
+    useState<MenuItemProps[]>(menuItems);
+  const searchRef = useRef<HTMLInputElement>(null);
+  const [showSearch, setShowSearch] = useState(false);
+
+  useEffect(() => {
+    if (showSearch) {
+      searchRef.current?.focus();
+    }
+  }, [showSearch]);
+
+  const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilteredMenuItems(
+      menuItems.filter((item) => {
+        try {
+          const regex = new RegExp(e.target.value, "i");
+          return regex.test(item.displayName) || regex.test(item.name);
+        } catch {
+          // If invalid regex, fallback to substring match
+          const val = e.target.value.toLowerCase();
+          return (
+            item.displayName.toLowerCase().includes(val) ||
+            item.name.toLowerCase().includes(val)
+          );
+        }
+      }),
+    );
+  };
+  return (
+    <StyledSiderCard>
+      <Container>
+        <TitleContainer>
+          <Body weight="medium">{title} </Body>
+          <IconButton onClick={() => setShowSearch(!showSearch)}>
+            <Icon glyph="MagnifyingGlass" />
+          </IconButton>
+        </TitleContainer>
+        {showSearch && (
+          <SearchInput
+            ref={searchRef}
+            aria-labelledby={title}
+            onChange={onSearchChange}
+            placeholder="Search build variants regex"
+          />
+        )}
+        <Description>
+          Use Shift + Click to edit multiple variants simultaneously.
+        </Description>
+        <Divider />
+      </Container>
+      <ScrollableBuildVariantContainer>
+        {filteredMenuItems.map(({ displayName, name, taskCount }) => {
+          const isSelected = selectedMenuItems.includes(name);
+          return (
+            <BuildVariant
+              key={name}
+              data-cy={dataCy}
+              data-selected={isSelected}
+              isSelected={isSelected}
+              onClick={onClick(name)}
+            >
+              <VariantName>
+                <Body
+                  weight={isSelected || taskCount > 0 ? "medium" : "regular"}
+                >
+                  {displayName}
+                </Body>
+              </VariantName>
+              {taskCount > 0 && (
+                <StyledBadge
+                  data-cy="task-count-badge"
+                  variant={isSelected ? Variant.DarkGray : Variant.LightGray}
+                >
+                  {taskCount}
+                </StyledBadge>
+              )}
+            </BuildVariant>
+          );
+        })}
+      </ScrollableBuildVariantContainer>
+    </StyledSiderCard>
+  );
+};
 
 interface VariantProps {
   isSelected: boolean;
 }
 
+const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
 const cardSidePadding = css`
   padding-left: ${size.xs};
   padding-right: ${size.xs};
