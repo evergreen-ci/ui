528ecf53040fcee1508ef2172ade6edcb883db67
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-10-08 16:24:38 -0400

    Revert "Prepare shell with eval"
    This reverts commit be8983457f71d697eb90a538cb3379247dbd41b1.

--- .evergreen/scripts/run-cypress.sh
+++ .evergreen/scripts/run-cypress.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-# Source shell environment
-eval "${PREPARE_SHELL}"
-
 # Allow spec filtering for an intentional patch.
 if [[ "${cypress_spec}" != "" ]]; then
   CYPRESS_CI=true CYPRESS_SKIP_VERIFY=true yarn cy:run --reporter junit --spec "${cypress_spec}"
