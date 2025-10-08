261b404691f95021ae031793a8ea1f48d83438e5
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-10-08 16:24:54 -0400

    Revert "Turn scripts into files"
    This reverts commit 49d2010317402dbd83d75c8e29e4089deff9bb0c.

--- .evergreen/scripts/run-cypress.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-# Allow spec filtering for an intentional patch.
-if [[ "${cypress_spec}" != "" ]]; then
-  CYPRESS_CI=true CYPRESS_SKIP_VERIFY=true yarn cy:run --reporter junit --spec "${cypress_spec}"
-else
-  CYPRESS_CI=true CYPRESS_SKIP_VERIFY=true yarn cy:run --reporter junit
-fi
