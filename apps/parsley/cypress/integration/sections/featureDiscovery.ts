00127d4bae9a538802d3c320075a6284556cc609
Sophie Stadler <sophie.stadler@mongodb.com>
2025-10-10 15:38:23 -0400

    Fix parsley e2e

--- apps/parsley/cypress/integration/sections/featureDiscovery.ts
+++ apps/parsley/cypress/integration/sections/featureDiscovery.ts
@@ -6,11 +6,11 @@ describe("sections feature discovery", () => {
     cy.visit(
       "/resmoke/mongodb_mongo_master_enterprise_amazon_linux2_arm64_all_feature_flags_jsCore_patch_9801cf147ed208ce4c0ff8dff4a97cdb216f4c22_65f06bd09ccd4eaaccca1391_24_03_12_14_51_29/0/job0/all",
     );
-    cy.dataCy("sections-feature-modal").should("not.be.visible");
+    cy.dataCy("sections-feature-modal").should("not.exist");
     cy.visit(
       "/test/spruce_ubuntu1604_check_codegen_d54e2c6ede60e004c48d3c4d996c59579c7bbd1f_22_03_02_15_41_35/0/JustAFakeTestInALonelyWorld",
     );
-    cy.dataCy("sections-feature-modal").should("not.be.visible");
+    cy.dataCy("sections-feature-modal").should("not.exist");
     cy.visit(taskLog);
     cy.dataCy("sections-feature-modal").should("be.visible");
   });
