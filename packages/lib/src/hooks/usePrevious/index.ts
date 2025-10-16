f78ba5eb7be966cf89e1d085b8be67134adda8bb
minnakt <minna.kt@mongodb.com>
2025-10-16 11:56:59 -0400

    Add back comment

--- packages/lib/src/hooks/usePrevious/index.ts
+++ packages/lib/src/hooks/usePrevious/index.ts
@@ -1,10 +1,15 @@
 import { useEffect, useRef } from "react";
 
-export const usePrevious = <T>(value: T): T | undefined => {
+/**
+ * `usePrevious` is a hook that returns the previous value of a state.
+ * @param state - the value to track
+ * @returns the previous value of the state
+ */
+export const usePrevious = <T>(state: T): T | undefined => {
   const ref = useRef<T>();
   useEffect(() => {
-    ref.current = value;
-  });
+    ref.current = state;
+  }, [state]);
   // This is not correct use of refs, but it's not possible to fix until DEVPROD-14178 is done
   // because it throws errors that are not easy to debug.
   return ref.current;
