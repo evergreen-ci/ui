a6f6c10d349a45028c5bc3748cfdedf277a7f6cd
Sophie Stadler <sophie.stadler@mongodb.com>
2025-10-23 13:07:27 -0500

    DEVPROD-23361: Fix image overflow in Parsley AI modal (#1137)

---
+++ apps/parsley/src/components/ParsleyAIModal/ParsleyAIModal.stories.tsx
@@ -0,0 +1,13 @@
+import { CustomMeta, CustomStoryObj } from "@evg-ui/lib/test_utils/types";
+import { ParsleyAIModal } from ".";
+
+export default {
+  component: ParsleyAIModal,
+} satisfies CustomMeta<typeof ParsleyAIModal>;
+
+export const Default = {
+  args: {
+    open: true,
+    setOpen: () => {},
+  },
+} satisfies CustomStoryObj<typeof ParsleyAIModal>;
