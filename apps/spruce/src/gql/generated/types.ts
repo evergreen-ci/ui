9034fbc22086ec6bc9edf26dcbc3fd49ca34bea6
Sophie Stadler <sophie.stadler@mongodb.com>
2025-10-30 09:36:25 -0400

    Add diffs to codechanges table

--- apps/spruce/src/gql/generated/types.ts
+++ apps/spruce/src/gql/generated/types.ts
@@ -1126,6 +1126,7 @@ export type FileDiff = {
   additions: Scalars["Int"]["output"];
   deletions: Scalars["Int"]["output"];
   description: Scalars["String"]["output"];
+  diff: Scalars["String"]["output"];
   diffLink: Scalars["String"]["output"];
   fileName: Scalars["String"]["output"];
 };
@@ -5225,7 +5226,7 @@ export type FileDiffsFragment = {
   additions: number;
   deletions: number;
   description: string;
-  diffLink: string;
+  diff: string;
   fileName: string;
 };
 
@@ -5246,7 +5247,7 @@ export type ModuleCodeChangeFragment = {
     additions: number;
     deletions: number;
     description: string;
-    diffLink: string;
+    diff: string;
     fileName: string;
   }>;
 };
@@ -8250,7 +8251,7 @@ export type CodeChangesQuery = {
         additions: number;
         deletions: number;
         description: string;
-        diffLink: string;
+        diff: string;
         fileName: string;
       }>;
     }>;
