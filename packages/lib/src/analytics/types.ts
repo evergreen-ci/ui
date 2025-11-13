7c24925e995f6df1a92e396e28b1cf9b3bb31cdf
Sophie Stadler <sophie.stadler@mongodb.com>
2025-11-13 13:21:51 -0500

    Reduce spruce violations

--- packages/lib/src/analytics/types.ts
+++ packages/lib/src/analytics/types.ts
@@ -51,5 +51,8 @@ export interface Analytics<Action extends ActionType> {
 /**
  * `ExtractAnalyticsSendEvent` is a utility type that can be used to extract the sendEvent function from an analytics hook.
  */
-export type ExtractAnalyticsSendEvent<A extends () => Analytics<ActionType>> =
-  ReturnType<A>["sendEvent"];
+export type ExtractAnalyticsSendEvent<A> = A extends () => Analytics<
+  infer Action
+>
+  ? Analytics<Action>["sendEvent"]
+  : never;
