81c59e3a14ec7cb9f01f8feb9f4a94a385e1e3ac
Sophie Stadler <sophie.stadler@mongodb.com>
2025-11-18 12:20:37 -0500

    Fix tests

--- apps/spruce/cypress/integration/version/unscheduled_patch/code_changes_table.ts
+++ apps/spruce/cypress/integration/version/unscheduled_patch/code_changes_table.ts
@@ -8,15 +8,17 @@ describe("Code Changes Table", () => {
   it("Should display at least one table when there are code changes", () => {
     cy.dataCy("code-changes-table").should("exist");
   });
-  it("Should link to code changes when they exist", () => {
-    cy.dataCy("fileLink")
-      .should("have.attr", "href")
-      .and("include", `filediff/${patchId}`);
+
+  it("Should link to code changes for the whole diff", () => {
     cy.dataCy("html-diff-btn")
       .should("have.attr", "href")
       .and("include", `filediff/${patchId}`);
     cy.dataCy("raw-diff-btn")
       .should("have.attr", "href")
       .and("include", `rawdiff/${patchId}`);
+  });
+
+  it("Should display collapsed code changes in table", () => {
+    cy.get("button[aria-label='Expand row']").should("have.length", 4);
   });
 });
