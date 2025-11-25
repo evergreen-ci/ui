ecfd76a5abf0ca431e54b13cc47e5e740cf5382d
Mohamed Khelif <mohamed.khelif@mongodb.com>
2025-11-25 16:16:38 -0500

    Add usePageVisibility analytics to parsley

--- apps/parsley/src/pages/LogDrop.tsx
+++ apps/parsley/src/pages/LogDrop.tsx
@@ -1,9 +1,11 @@
+import { usePageVisibilityAnalytics } from "@evg-ui/lib/analytics/hooks/usePageVisibilityAnalytics";
 import { usePageTitle } from "@evg-ui/lib/hooks/usePageTitle";
 import LogWindow from "components/LogWindow";
 import { useLogContext } from "context/LogContext";
 import FileDropper from "./LogDrop/FileDropper";
 
 const LogDrop = () => {
+  usePageVisibilityAnalytics({ identifier: "LogDrop" });
   const { hasLogs, logMetadata } = useLogContext();
   usePageTitle(`${hasLogs ? logMetadata?.fileName : "Upload logs"} | Parsley`);
   return hasLogs ? <LogWindow /> : <FileDropper />;
