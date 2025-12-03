d583f748fd66bb058868e56a65720632b020e668
Sophie Stadler <sophie.stadler@mongodb.com>
2025-12-03 11:25:16 -0500

    Add test cases

--- apps/spruce/cypress/integration/version/unscheduled_patch/code_changes_table.ts
+++ apps/spruce/cypress/integration/version/unscheduled_patch/code_changes_table.ts
@@ -11,10 +11,14 @@ describe("Code Changes Table", () => {
   it("Should link to code changes when they exist", () => {
     cy.dataCy("fileLink")
       .should("have.attr", "href")
-      .and("include", `/version/${patchId}/file-diff`);
+      .and("include", `/version/${patchId}/file-diff`)
+      .and("include", "commit_number=")
+      .and("include", "file_name=")
+      .and("include", "patch_number=");
     cy.dataCy("html-diff-btn")
       .should("have.attr", "href")
-      .and("include", `/version/${patchId}/diff`);
+      .and("include", `/version/${patchId}/diff`)
+      .and("include", "patch_number=");
     cy.dataCy("raw-diff-btn")
       .should("have.attr", "href")
       .and("include", `rawdiff/${patchId}`);
