59f26bf55ddb59c43695418d4a8991ec9a9f6157
Sophie Stadler <sophie.stadler@mongodb.com>
2025-12-10 16:13:48 -0500

    Add search details

--- apps/spruce/src/pages/waterfall/Empty/index.tsx
+++ apps/spruce/src/pages/waterfall/Empty/index.tsx
@@ -1,9 +1,10 @@
 import { BasicEmptyState } from "@leafygreen-ui/empty-state";
+import { VERSION_SEARCH_LIMIT } from "../constants";
 import { EmptyGraphic } from "./EmptyGraphic";
 
 export const Empty: React.FC = () => (
   <BasicEmptyState
-    description="Evergreen found no commits matching the applied filters."
+    description={`Evergreen found no builds matching the applied filters in the ${VERSION_SEARCH_LIMIT} most recent commits.`}
     graphic={<EmptyGraphic />}
     title="No Results Found"
   />
