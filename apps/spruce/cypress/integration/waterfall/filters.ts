82f1ff9fb21e73ba3cecd01498ccb2aa665095e8
Sophie Stadler <sophie.stadler@mongodb.com>
2025-12-12 09:33:49 -0500

    DEVPROD-15644: Add empty waterfall view (#1237)

--- apps/spruce/cypress/integration/waterfall/filters.ts
+++ apps/spruce/cypress/integration/waterfall/filters.ts
@@ -18,18 +18,11 @@ describe("requester filtering", () => {
     cy.visit("/project/spruce/waterfall");
   });
 
-  it("filters on periodic builds and trigger", () => {
+  it("filters on periodic builds and shows an empty state", () => {
     cy.dataCy("inactive-versions-button").first().contains("1");
     cy.dataCy("requester-filter").click();
     cy.dataCy("ad_hoc-option").click();
-    cy.dataCy("inactive-versions-button").first().contains("6");
-    cy.dataCy("version-label-active").should("have.length", 0);
-
-    cy.dataCy("requester-filter").click();
-    cy.dataCy("trigger_request-option").click();
-    cy.dataCy("inactive-versions-button").first().contains("5");
-    cy.dataCy("version-label-active").should("have.length", 1);
-    cy.dataCy("version-label-active").contains("Triggered by:");
+    cy.contains("No Results Found").should("be.visible");
   });
 
   it("filters on git tags and fetches more from the server", () => {
