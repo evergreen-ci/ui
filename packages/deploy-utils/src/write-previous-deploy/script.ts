aa260a03da51efe71ea09b6a799b110f9a2e2e56
Sophie Stadler <sophie.stadler@mongodb.com>
2026-01-08 17:21:23 -0500

    Reolcate VITE_SCRIPT_MODE definition

--- packages/deploy-utils/src/write-previous-deploy/script.ts
+++ packages/deploy-utils/src/write-previous-deploy/script.ts
@@ -4,8 +4,6 @@ import { existsSync, mkdirSync, writeFileSync } from "fs";
 import { getAppToDeploy } from "../utils/environment";
 import { getCurrentlyDeployedCommit } from "../utils/git";
 
-process.env.VITE_SCRIPT_MODE = "1";
-
 const dirName = "bin";
 const app = getAppToDeploy();
 const currentlyDeployedCommit = await getCurrentlyDeployedCommit(app);
