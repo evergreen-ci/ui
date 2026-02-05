49b766eda09adf2060c6d8d8c2486f767413485f
Mohamed Khelif <mohamed.khelif@mongodb.com>
2026-02-05 16:55:36 -0500

    Add disclaimer about only working for test logs

--- apps/parsley/src/components/DetailsMenu/DetailsMenuCard/Toggles/IncludeTimestampsToggle/index.tsx
+++ apps/parsley/src/components/DetailsMenu/DetailsMenuCard/Toggles/IncludeTimestampsToggle/index.tsx
@@ -1,21 +1,29 @@
 import { usePreferencesAnalytics } from "analytics";
+import { LogTypes } from "constants/enums";
 import { useLogContext } from "context/LogContext";
 import BaseToggle from "../BaseToggle";
 
 const IncludeTimestampsToggle: React.FC = () => {
   const { sendEvent } = usePreferencesAnalytics();
-  const { preferences } = useLogContext();
+  const { logMetadata, preferences } = useLogContext();
   const { includeTimestamps, setIncludeTimestamps } = preferences;
+
+  const isSupported = logMetadata?.logType === LogTypes.EVERGREEN_TEST_LOGS;
 
   return (
     <BaseToggle
       data-cy="include-timestamps-toggle"
+      disabled={!isSupported}
       label="Include Timestamps"
       onChange={(value) => {
         sendEvent({ name: "Toggled include timestamps", on: value });
         setIncludeTimestamps(value);
       }}
-      tooltip="Whether timestamps are included at the beginning of log lines. Changing this setting will reload the page to re-download the log."
+      tooltip={
+        isSupported
+          ? "Whether timestamps are included at the beginning of log lines. Changing this setting will reload the page to re-download the log."
+          : "This option is only available for Evergreen test logs."
+      }
       value={includeTimestamps}
     />
   );
