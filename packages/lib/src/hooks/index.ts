4c3eaa2ac64ce4aee5ca51ff30194da624c3bc83
minnakt <minna.kt@mongodb.com>
2026-02-06 14:26:56 -0500

    fix: delete import

--- packages/lib/src/hooks/index.ts
+++ packages/lib/src/hooks/index.ts
@@ -3,5 +3,4 @@ export { useKeyboardShortcut } from "./useKeyboardShortcut";
 export { useOnClickOutside } from "./useOnClickOutside";
 export { usePageTitle } from "./usePageTitle";
 export { usePrevious } from "./usePrevious";
-export { useQueryCompleted } from "./useQueryCompleted";
 export { useQueryParam, useQueryParams } from "./useQueryParam";
