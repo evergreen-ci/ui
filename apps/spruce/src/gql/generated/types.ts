6841899c62d5a804607f1546a28af37e7fb87de0
annie.black <annie.black@mongodb.com>
2026-02-13 13:39:41 -0500

    fix

--- apps/spruce/src/gql/generated/types.ts
+++ apps/spruce/src/gql/generated/types.ts
@@ -4219,6 +4219,7 @@ export type Task = {
   generateTask?: Maybe<Scalars["Boolean"]["output"]>;
   generatedBy?: Maybe<Scalars["String"]["output"]>;
   generatedByName?: Maybe<Scalars["String"]["output"]>;
+  generator?: Maybe<Task>;
   hasTestResults: Scalars["Boolean"]["output"];
   hostId?: Maybe<Scalars["String"]["output"]>;
   id: Scalars["String"]["output"];
