fa73baa36b295346d294172c9fa6d6c4335f1b44
Sophie Stadler <sophie.stadler@mongodb.com>
2026-03-05 14:17:30 -0500

    Remove modal

--- apps/parsley/src/pages/LogDrop.tsx
+++ apps/parsley/src/pages/LogDrop.tsx
@@ -1,33 +1,13 @@
-import { useEffect } from "react";
 import { usePageTitle } from "@evg-ui/lib/hooks/usePageTitle";
 import LogWindow from "components/LogWindow";
-import { NavigationWarningModal } from "components/NavigationWarningModal";
 import { useLogContext } from "context/LogContext";
 import FileDropper from "./LogDrop/FileDropper";
 
 const LogDrop = () => {
-  const { hasLogs, isUploadedLog, logMetadata } = useLogContext();
+  const { hasLogs, logMetadata } = useLogContext();
   usePageTitle(`${hasLogs ? logMetadata?.fileName : "Upload logs"} | Parsley`);
 
-  useEffect(() => {
-    const handleBeforeUnload = (e: BeforeUnloadEvent) => {
-      if (isUploadedLog) {
-        e.preventDefault();
-      }
-    };
-
-    window.addEventListener("beforeunload", handleBeforeUnload);
-    return () => {
-      window.removeEventListener("beforeunload", handleBeforeUnload);
-    };
-  }, [isUploadedLog]);
-
-  return (
-    <>
-      <NavigationWarningModal shouldBlock={isUploadedLog} />
-      {hasLogs ? <LogWindow /> : <FileDropper />}
-    </>
-  );
+  return hasLogs ? <LogWindow /> : <FileDropper />;
 };
 
 export default LogDrop;
