0b30b77dee8db42b8aad16f67897b3c1c0a134b7
Sophia Yang <sophia.yang@mongodb.com>
2026-03-24 13:23:54 -0400

    add warning when max unexpirable hosts

--- apps/spruce/src/components/Spawn/getFormSchema.tsx
+++ apps/spruce/src/components/Spawn/getFormSchema.tsx
@@ -321,6 +321,10 @@ export const getExpirationDetailsSchema = ({
     },
     uiSchema: {
       "ui:tooltipTitle": noExpirationCheckboxTooltip ?? "",
+      "ui:warnings":
+        disableExpirationCheckbox && noExpirationCheckboxTooltip
+          ? [noExpirationCheckboxTooltip]
+          : [],
       noExpiration: {
         "ui:enumDisabled": disableExpirationCheckbox ? [true] : null,
         "ui:data-cy": "expirable-radio-box",
