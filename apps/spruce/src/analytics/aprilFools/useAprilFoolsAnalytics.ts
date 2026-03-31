167d2194d580b2ed759e21c95ad2a4743eeb2050
Sophia Yang <sophia.yang@mongodb.com>
2026-03-31 13:22:15 -0400

    images

--- apps/spruce/src/analytics/aprilFools/useAprilFoolsAnalytics.ts
+++
@@ -1,7 +0,0 @@
-import { useAnalyticsRoot } from "@evg-ui/lib/analytics/hooks";
-import { AnalyticsIdentifier } from "analytics/types";
-
-type Action = { name: "Used 2024 Boilerplate!" };
-
-export const useAprilFoolsAnalytics = () =>
-  useAnalyticsRoot<Action, AnalyticsIdentifier>("April Fools");
