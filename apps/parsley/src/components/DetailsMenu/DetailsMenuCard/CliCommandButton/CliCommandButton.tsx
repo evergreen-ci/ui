026f832ad5cde75ce8dedb7947759ad99e0bf9b5
Sophia Yang <sophia.yang@mongodb.com>
2026-04-10 15:52:00 -0400

    another refactor

--- apps/parsley/src/components/DetailsMenu/DetailsMenuCard/CliCommandButton/CliCommandButton.tsx
+++ apps/parsley/src/components/DetailsMenu/DetailsMenuCard/CliCommandButton/CliCommandButton.tsx
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
 import { Copyable } from "@leafygreen-ui/copyable";
-import { size } from "@evg-ui/lib/constants/tokens";
+import { Origin } from "@evg-ui/lib/constants/logURLTemplates";
 import { LogTypes } from "constants/enums";
 import { useLogContext } from "context/LogContext";
 import { LogMetadata } from "context/LogContext/types";
@@ -14,17 +14,24 @@ const CliCommandButton: React.FC = () => {
   }
 
   return (
-    <Container>
-      <StyledCopyable
-        data-cy="cli-command-copyable"
-        label="Fetch the log via Evergreen CLI"
-      >
-        {command}
-      </StyledCopyable>
-    </Container>
+    <StyledCopyable
+      data-cy="cli-command-copyable"
+      label="Fetch the log via Evergreen CLI"
+    >
+      {command}
+    </StyledCopyable>
   );
 };
 
+const originToCLIType: Record<Origin, string> = {
+  [Origin.Agent]: "agent_log",
+  [Origin.All]: "all_logs",
+  [Origin.System]: "system_log",
+  [Origin.Task]: "task_log",
+};
+
+// Based on documentation at
+// https://docs.devprod.prod.corp.mongodb.com/parsley/Downloading-Logs.
 const getCliCommand = (logMetadata?: LogMetadata): string | null => {
   if (!logMetadata) {
     return null;
@@ -33,35 +40,21 @@ const getCliCommand = (logMetadata?: LogMetadata): string | null => {
   if (!logType || !taskID || execution == null) {
     return null;
   }
-
-  // Based on documentation at
-  // https://docs.devprod.prod.corp.mongodb.com/parsley/Downloading-Logs.
   if (logType === LogTypes.EVERGREEN_TASK_LOGS) {
-    const origin = logMetadata?.origin?.toLowerCase() || "task";
-    const originToCLIType: Record<string, string> = {
-      agent: "agent_log",
-      all: "all_logs",
-      system: "system_log",
-      task: "task_log",
-    };
-    const taskType = originToCLIType[origin] ?? "task_log";
+    const origin =
+      (logMetadata?.origin?.toLowerCase() as Origin) || Origin.Task;
+    const taskType = originToCLIType[origin];
     return `evergreen task build TaskLogs --task_id ${taskID} --execution ${execution} --type ${taskType} --o output.txt`;
   }
   if (logType === LogTypes.EVERGREEN_TEST_LOGS) {
-    if (!logPath || logPath === "") {
+    if (!logPath) {
       return null;
     }
     return `evergreen task build TestLogs --task_id ${taskID} --execution ${execution} --log_path ${logPath} --o output.txt`;
   }
-
   // Unsupported log type.
   return null;
 };
-
-const Container = styled.div`
-  margin-top: ${size.xs};
-  width: 100%;
-`;
 
 const StyledCopyable = styled(Copyable)`
   width: 100%;
