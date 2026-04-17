b43af80658fc4464601a314925ac6a77018aa421
Sophia Yang <sophia.yang@mongodb.com>
2026-04-17 15:22:07 -0400

    delete extra flag from merge

--- apps/spruce/src/constants/featureFlags.ts
+++ apps/spruce/src/constants/featureFlags.ts
@@ -1,5 +1,4 @@
 import { isEndUserProduction } from "utils/environmentVariables";
 
-export const showRecreatableTaskEnvironments = !isEndUserProduction();
-// TODO DEVPROD-8355: remove feature flag and its references
+// TODO DEVPROD-31534: remove feature flag and its references
 export const showNewProjectNavigation = !isEndUserProduction();
