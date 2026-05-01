7d5a1c2ca66347b70eaaa49dc2820ba806e14377
minnakt <minna.kt@mongodb.com>
2026-05-01 15:36:32 -0400

    delete old changes to ease merge conflicts

--- apps/spruce/playwright/tests/adminSettings/utils.ts
+++
@@ -1,8 +0,0 @@
-import { Page } from "@playwright/test";
-import { expect } from "../../fixtures";
-
-export const save = async (page: Page) => {
-  const saveButton = page.getByTestId("save-settings-button");
-  await expect(saveButton).toHaveAttribute("aria-disabled", "false");
-  await saveButton.click();
-};
