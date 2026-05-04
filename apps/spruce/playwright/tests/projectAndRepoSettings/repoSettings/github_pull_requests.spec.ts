77bab8bbbe6e3a6d4d2c3de5e3084f3eb0e5890a
minnakt <minna.kt@mongodb.com>
2026-05-04 16:30:23 -0400

    don't use repo for project test

--- apps/spruce/playwright/tests/projectAndRepoSettings/repoSettings/github_pull_requests.spec.ts
+++ apps/spruce/playwright/tests/projectAndRepoSettings/repoSettings/github_pull_requests.spec.ts
@@ -2,50 +2,16 @@ import { test, expect } from "../../../fixtures";
 import { clickRadio, validateToast } from "../../../helpers";
 import {
   getProjectSettingsRoute,
+  projectUseRepoEnabled,
   ProjectSettingsTabRoutes,
-  project,
+  repo,
+  getRepoSettingsRoute,
 } from "../constants";
 import { expectSaveButtonEnabled, save } from "../utils";
 
-test.describe("A project that has GitHub webhooks disabled", () => {
-  const destination = getProjectSettingsRoute(
-    "logkeeper",
-    ProjectSettingsTabRoutes.PullRequests,
-  );
-
-  test.beforeEach(async ({ authenticatedPage: page }) => {
-    await page.goto(destination);
-    await expectSaveButtonEnabled(page, false);
-  });
-
-  test("Pull Requests page shows a disabled webhooks banner when webhooks are disabled", async ({
-    authenticatedPage: page,
-  }) => {
-    const banner = page.getByTestId("disabled-webhook-banner");
-    await expect(banner).toBeVisible();
-    await expect(banner).toContainText(
-      "GitHub features are disabled because the Evergreen GitHub App is not",
-    );
-  });
-
-  test("Disables all interactive elements on the page", async ({
-    authenticatedPage: page,
-  }) => {
-    const settingsPage = page.getByTestId("project-settings-page");
-    const buttons = settingsPage.getByRole("button");
-    for (const button of await buttons.all()) {
-      await expect(button).toBeDisabled();
-    }
-    const inputs = page.locator("input");
-    for (const input of await inputs.all()) {
-      await expect(input).toBeDisabled();
-    }
-  });
-});
-
-test.describe("A project that has GitHub webhooks enabled", () => {
-  const destination = getProjectSettingsRoute(
-    project,
+test.describe("A repo that has GitHub webhooks enabled", () => {
+  const destination = getRepoSettingsRoute(
+    repo,
     ProjectSettingsTabRoutes.PullRequests,
   );
 
@@ -66,10 +32,6 @@ test.describe("A project that has GitHub webhooks enabled", () => {
   test("Saving a patch definition should hide the error banner, show a success toast and display disabled patch definitions for the repo on the project page", async ({
     authenticatedPage: page,
   }) => {
-    const radioBox = page.getByTestId("pr-testing-enabled-radio-box");
-    const enabledRadio = radioBox.getByRole("radio", { name: "Enabled" });
-    await clickRadio(enabledRadio);
-
     const errorBanner = page.getByTestId("error-banner").filter({
       hasText:
         "A GitHub Patch Definition must be specified for this feature to run.",
@@ -85,6 +47,26 @@ test.describe("A project that has GitHub webhooks enabled", () => {
     await expectSaveButtonEnabled(page, true);
 
     await save(page);
-    await validateToast(page, "success", "Successfully updated project");
+    await validateToast(page, "success", "Successfully updated repo");
+
+    await page.goto(
+      getProjectSettingsRoute(
+        projectUseRepoEnabled,
+        ProjectSettingsTabRoutes.PullRequests,
+      ),
+    );
+
+    const patchDefAccordion = page.getByText("Repo Patch Definition 1");
+    await patchDefAccordion.click();
+
+    const variantTagsInput = page.getByTestId("variant-tags-input");
+    await expect(variantTagsInput).toHaveValue("vtag");
+    await expect(variantTagsInput).toBeDisabled();
+
+    const taskTagsInput = page.getByTestId("task-tags-input");
+    await expect(taskTagsInput).toHaveValue("ttag");
+    await expect(taskTagsInput).toBeDisabled();
+
+    await expect(errorBanner).toBeHidden();
   });
 });
