c8c19c797a48f91144ca6d2eb085dbe56c6fe8ff
minnakt <minna.kt@mongodb.com>
2026-05-06 11:36:37 -0400

    fix: low max age for `commit.txt` file

--- packages/deploy-utils/src/utils/s3.ts
+++ packages/deploy-utils/src/utils/s3.ts
@@ -8,12 +8,15 @@ export const pushToS3 = (bucket: string) => {
       `aws s3 sync ${buildDir}/ s3://"${bucket}"/ --follow-symlinks --delete --exclude .env-cmdrc.json`,
       { stdio: "inherit" },
     );
-    // Specifically upload index.html with a very low max age to prevent Cloudfront from caching it.
+    // Specifically upload index.html and commit.txt with very low max age to prevent Cloudfront from
+    // caching them.
     // The rest of our assets are hashed, so they can be cached safely as they are unique per-deploy.
-    execSync(
-      `aws s3 cp ${buildDir}/index.html s3://"${bucket}"/ --cache-control 'max-age=60, public'`,
-      { stdio: "inherit" },
-    );
+    for (const file of ["index.html", "commit.txt"]) {
+      execSync(
+        `aws s3 cp ${buildDir}/${file} s3://"${bucket}"/ --cache-control 'max-age=60, public'`,
+        { stdio: "inherit" },
+      );
+    }
     console.log("Successfully deployed to S3");
   } catch (e) {
     throw Error("Deployment to S3 failed", { cause: e });
