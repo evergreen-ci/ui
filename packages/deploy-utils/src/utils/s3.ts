53f7149f8afbe45cc976fadbe58592c03f0fcdcc
minnakt <minna.kt@mongodb.com>
2026-05-06 11:50:50 -0400

    make `doNotCacheFiles`

--- packages/deploy-utils/src/utils/s3.ts
+++ packages/deploy-utils/src/utils/s3.ts
@@ -1,5 +1,7 @@
 import { execSync } from "child_process";
 
+const doNotCacheFiles = ["index.html", "commit.txt"];
+
 export const pushToS3 = (bucket: string) => {
   console.log("Attempting to deploy to S3");
   const buildDir = "dist";
@@ -8,10 +10,9 @@ export const pushToS3 = (bucket: string) => {
       `aws s3 sync ${buildDir}/ s3://"${bucket}"/ --follow-symlinks --delete --exclude .env-cmdrc.json`,
       { stdio: "inherit" },
     );
-    // Specifically upload index.html and commit.txt with very low max age to prevent Cloudfront from
-    // caching them.
+    // Specifically upload these files with very low max age to prevent Cloudfront from caching them.
     // The rest of our assets are hashed, so they can be cached safely as they are unique per-deploy.
-    for (const file of ["index.html", "commit.txt"]) {
+    for (const file of doNotCacheFiles) {
       execSync(
         `aws s3 cp ${buildDir}/${file} s3://"${bucket}"/ --cache-control 'max-age=60, public'`,
         { stdio: "inherit" },
