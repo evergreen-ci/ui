d4113d13abf6680cca8ef5712ca3cada6658101e
minnakt <47064971+minnakt@users.noreply.github.com>
2026-05-07 15:56:36 -0400

    DEVPROD-32262: Clarify announcement banner styles (#1637)

--- apps/spruce/src/components/Banners/index.ts
+++ apps/spruce/src/components/Banners/index.ts
@@ -9,3 +9,4 @@ export { ProjectBanner } from "./ProjectBanner";
 export { RepotrackerBanner } from "./RepotrackerBanner";
 export { WarningBanner } from "./WarningBanner";
 export { ErrorBanner } from "./ErrorBanner";
+export { bannerThemeToLabelMap } from "./AnnouncementBannerThemes";
