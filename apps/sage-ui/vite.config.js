4e7b353f288fc51495c305f84e8a7d1d0b79ea3a
minnakt <minna.kt@mongodb.com>
2026-05-27 17:45:29 -0400

    feat: evergreen tasks are working

---
+++ apps/sage-ui/vite.config.js
@@ -0,0 +1,25 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig, mergeConfig } from "vite";
+import { defineConfig as defineTestConfig } from "vitest/config";
+
+const viteConfig = defineConfig({
+  server: {
+    port: 5493,
+  },
+  plugins: [react()],
+  resolve: {
+    tsconfigPaths: true,
+  },
+});
+
+const vitestConfig = defineTestConfig({
+  test: {
+    environment: "jsdom",
+    globals: true,
+    outputFile: { junit: "./bin/vitest/junit.xml" },
+    reporters: ["default", ...(process.env.CI === "true" ? ["junit"] : [])],
+    setupFiles: ["./src/test_utils/setupTests.ts"],
+    include: ["src/**/*.test.{ts,tsx}"],
+  },
+});
+export default mergeConfig(viteConfig, vitestConfig);
