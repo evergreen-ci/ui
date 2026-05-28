4dcdd3c99fa2abee106ee1ff8f10afc681160dcc
minnakt <minna.kt@mongodb.com>
2026-05-28 15:38:00 -0400

    Revert "create new package"
    This reverts commit 05b41dd484fcb382ffdc1aebeb8fadd7d37bf334.

---
+++ apps/sage/src/test_utils/index.tsx
@@ -0,0 +1,25 @@
+import {
+  act,
+  fireEvent,
+  render,
+  renderHook,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+  within,
+  type RenderOptions,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+export {
+  act,
+  fireEvent,
+  render,
+  renderHook,
+  screen,
+  userEvent,
+  waitFor,
+  waitForElementToBeRemoved,
+  within,
+  type RenderOptions,
+};
