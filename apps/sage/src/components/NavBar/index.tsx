1c8e6f20881c6b0874e80b4f1aa6d3a58f967005
minnakt <minna.kt@mongodb.com>
2026-06-03 15:46:43 -0400

    DEVPROD-33404: Set up basic routes in Sage UI

---
+++ apps/sage/src/components/NavBar/index.tsx
@@ -0,0 +1,33 @@
+import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
+import { size, palette } from "@evg-ui/lib/constants/tokens";
+import {
+  getAgentDetailRoute,
+  getAgentRunsRoute,
+  routes,
+} from "constants/routes";
+
+export const NavBar: React.FC = () => (
+  <NavContainer>
+    <PrimaryLink to={routes.home}>Home</PrimaryLink>
+    <PrimaryLink to={getAgentDetailRoute("sage-bot")}>Agent Info</PrimaryLink>
+    <PrimaryLink to={getAgentRunsRoute("sage-bot", "abcdefg")}>
+      Agent Runs
+    </PrimaryLink>
+  </NavContainer>
+);
+
+const NavContainer = styled.nav`
+  display: flex;
+  align-items: center;
+  gap: ${size.m};
+  flex-shrink: 0;
+
+  background-color: ${palette.gray.light3};
+  padding: ${size.s};
+`;
+
+const PrimaryLink = styled(Link)`
+  color: ${palette.gray.dark3};
+  text-decoration: none;
+`;
---
+++ apps/sage/src/pages/Home/index.tsx
@@ -0,0 +1,8 @@
+import { TestComponent } from "components/TestComponent";
+
+export const HomePage: React.FC = () => (
+  <div>
+    <p>Home page</p>
+    <TestComponent />
+  </div>
+);
