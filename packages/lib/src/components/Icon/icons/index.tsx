14154f9e84e096a8f88a0e4fc889a0c02f9bdea3
Sophie Stadler <sophie.stadler@mongodb.com>
2026-07-20 15:39:46 -0400

    More cleanup

--- packages/lib/src/components/Icon/icons/index.tsx
+++ packages/lib/src/components/Icon/icons/index.tsx
@@ -4,8 +4,6 @@ export { ParsleyLogo } from "./logos/ParsleyLogo";
 export { KnownFailure } from "./statuses/KnownFailure";
 
 export { ArrowWithCircle } from "./ArrowWithCircle";
-export { ClockCheckmark } from "./ClockCheckmark";
-export { ClockX } from "./ClockX";
 export { ClosedEye } from "./ClosedEye";
 export { Expand } from "./Expand";
 export { Ignored } from "./Ignored";
