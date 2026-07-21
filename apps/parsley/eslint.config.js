9a1955c62e60035be0d289d916b99a7104125d89
minnakt <47064971+minnakt@users.noreply.github.com>
2026-07-21 14:11:09 -0400

    DEVPROD-29652: Remove `eslint-plugin-sort-keys-plus` (#1810)

--- apps/parsley/eslint.config.js
+++ apps/parsley/eslint.config.js
@@ -1,25 +1,3 @@
-import { fixupPluginRules } from "@eslint/compat";
-import { defineConfig } from "eslint/config";
-import * as sortKeysPlugin from "eslint-plugin-sort-keys-plus";
-import baseConfig, { errorIfStrict } from "@evg-ui/eslint-config";
+import baseConfig from "@evg-ui/eslint-config";
 
-export default defineConfig(
-  ...baseConfig,
-  // Sort Keys File ESLint (eslint-plugin-sort-keys-plus) settings.
-  // TODO DEVPROD-6890: This plugin should be dropped because the project has been abandoned.
-  // It may potentially be replaced by Perfectionist's sort-objects rule (https://perfectionist.dev/rules/sort-objects).
-  {
-    name: "sort-keys-plus/rules",
-    files: ["src/**/*.ts?(x)"],
-    plugins: {
-      "sort-keys-plus": fixupPluginRules(sortKeysPlugin),
-    },
-    rules: {
-      "sort-keys-plus/sort-keys": [
-        errorIfStrict,
-        "asc",
-        { allowLineSeparatedGroups: true, natural: true },
-      ],
-    },
-  },
-);
+export default baseConfig;
