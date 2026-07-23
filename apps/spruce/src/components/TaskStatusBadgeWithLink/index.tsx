5ea8b0704f8f1edafab6d19a45242bafec8d118a
minnakt <minna.kt@mongodb.com>
2026-07-23 17:25:42 -0400

    proposal: icons on badge hover

--- apps/spruce/src/components/TaskStatusBadgeWithLink/index.tsx
+++ apps/spruce/src/components/TaskStatusBadgeWithLink/index.tsx
@@ -1,5 +1,9 @@
+import styled from "@emotion/styled";
+import { Icon } from "@leafygreen-ui/icon";
+import { palette } from "@leafygreen-ui/palette";
 import { Link } from "react-router-dom";
 import TaskStatusBadge from "@evg-ui/lib/components/Badge/TaskStatusBadge";
+import { size, transitionDuration } from "@evg-ui/lib/constants/tokens";
 import { TaskStatus } from "@evg-ui/lib/types/task";
 import { getTaskRoute } from "constants/routes";
 import { TaskTab } from "types/task";
@@ -27,15 +31,32 @@ const TaskStatusBadgeWithLink: React.FC<TaskStatusBadgeWithLinkProps> = ({
   }
 
   return (
-    <Link
+    <StyledLink
       to={getTaskRoute(id, {
         execution,
         tab: linkedTab,
       })}
     >
       <TaskStatusBadge status={status as TaskStatus} {...rest} />
-    </Link>
+      <Icon className="link-icon" fill={palette.blue.base} glyph="OpenNewTab" />
+    </StyledLink>
   );
 };
+
+const StyledLink = styled(Link)`
+  display: inline-flex;
+  align-items: center;
+  gap: ${size.xxs};
+  text-decoration: none;
+
+  .link-icon {
+    opacity: 0;
+    transition: opacity ${transitionDuration.default}ms ease-in;
+    flex-shrink: 0;
+  }
+  :hover .link-icon {
+    opacity: 1;
+  }
+`;
 
 export default TaskStatusBadgeWithLink;
