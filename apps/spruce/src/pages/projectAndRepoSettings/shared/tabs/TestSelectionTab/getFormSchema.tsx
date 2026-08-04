6c7de6939fa334b3f2fa94a1c4b272da93c05c8c
Aaron Hammer <aaronh@athammer.me>
2026-08-04 17:43:42 -0400

    Simplification

--- apps/spruce/src/pages/projectAndRepoSettings/shared/tabs/TestSelectionTab/getFormSchema.tsx
+++ apps/spruce/src/pages/projectAndRepoSettings/shared/tabs/TestSelectionTab/getFormSchema.tsx
@@ -14,6 +14,12 @@ const taskLevelLabels: Record<TaskLevelTestSelection, string> = {
     "Enabled for patches and mainline commits",
 };
 
+const taskLevelSettings = [
+  TaskLevelTestSelection.Disabled,
+  TaskLevelTestSelection.Patches,
+  TaskLevelTestSelection.PatchesAndMainline,
+];
+
 const taskLevelOption = (
   title: string,
   value: TaskLevelTestSelection | null,
@@ -25,19 +31,22 @@ const taskLevelOption = (
 
 const taskLevelOptions = (
   repoSetting?: TaskLevelTestSelection,
-): Array<SpruceFormProps["schema"]> => [
-  ...Object.entries(taskLevelLabels).map(([value, title]) =>
-    taskLevelOption(title, value as TaskLevelTestSelection),
-  ),
-  ...(repoSetting
-    ? [
-        taskLevelOption(
-          `Default to repo (${taskLevelLabels[repoSetting].toLowerCase()})`,
-          null,
-        ),
-      ]
-    : []),
-];
+): Array<SpruceFormProps["schema"]> => {
+  const options = taskLevelSettings.map((setting) =>
+    taskLevelOption(taskLevelLabels[setting], setting),
+  );
+
+  if (repoSetting) {
+    options.push(
+      taskLevelOption(
+        `Default to repo (${taskLevelLabels[repoSetting].toLowerCase()})`,
+        null,
+      ),
+    );
+  }
+
+  return options;
+};
 
 export const getFormSchema = ({
   canEnableTaskLevel,
