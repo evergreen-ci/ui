6ebc6fb4571f311fbaf57acadf1c12de727abba9
Terrence Keane <terrence.keane@mongodb.com>
2026-08-17 12:18:47 -0400

    review: merge className in FilterWrapper; use WordBreak all prop in TaskLink

--- packages/lib/src/components/Table/TablePopover/constants.tsx
+++ packages/lib/src/components/Table/TablePopover/constants.tsx
@@ -1,7 +1,9 @@
+import { cx } from "../../../utils/css";
 import styles from "./constants.module.css";
 
 export const DEFAULT_SPACING = 10;
 
-export const FilterWrapper: React.FC<React.ComponentPropsWithoutRef<"div">> = (
-  props,
-) => <div className={styles.filterWrapper} {...props} />;
+export const FilterWrapper: React.FC<React.ComponentPropsWithoutRef<"div">> = ({
+  className,
+  ...rest
+}) => <div className={cx(styles.filterWrapper, className)} {...rest} />;
