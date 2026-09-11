c112d71f4785004b6b46fed91b09de29b8995c5a
Terrence Keane <terrence.keane@mongodb.com>
2026-09-11 10:22:36 -0400

    Revert maxWidth on TaskSelector Combobox

--- apps/spruce/src/pages/variantHistory/TaskSelector.tsx
+++ apps/spruce/src/pages/variantHistory/TaskSelector.tsx
@@ -45,7 +45,6 @@ const TaskSelector: React.FC<TaskSelectorProps> = ({
         isDisabled={loading}
         isLoading={loading}
         label="Tasks"
-        maxWidth={500}
         menuTriggerAriaLabel="Tasks"
         onChange={(keys) => {
           sendEvent({ name: "Filtered by task" });
