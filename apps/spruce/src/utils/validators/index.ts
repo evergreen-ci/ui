40207404cb87c76a77f835a450d4d793ba753017
minnakt <47064971+minnakt@users.noreply.github.com>
2026-09-11 16:50:13 -0400

    DEVPROD-1020: Fix regex vulnerabilities (#1956)

--- apps/spruce/src/utils/validators/index.ts
+++ apps/spruce/src/utils/validators/index.ts
@@ -1,3 +1,5 @@
+import { toEscapedRegex } from "@evg-ui/lib/utils/string";
+
 /**
  * `validateDuration` tests if a provided string is a valid duration
  * @param duration - the duration to validate
@@ -47,8 +49,12 @@ const validateJira = (v: string) => new RegExp(jiraTicketNumberRegex).test(v);
  * @param url - the url to test
  * @returns - true if the provided url is a valid jira url, false otherwise
  */
-const validateJiraURL = (jiraURL: string, url: string): boolean =>
-  new RegExp(`^https://${jiraURL}/browse/${jiraTicketNumberRegex}$`).test(url);
+const validateJiraURL = (jiraURL: string, url: string): boolean => {
+  const escapedURL = toEscapedRegex(jiraURL);
+  return new RegExp(
+    `^https://${escapedURL}/browse/${jiraTicketNumberRegex}$`,
+  ).test(url);
+};
 
 /**
  * `validateURL` tests if a provided url is a valid url
@@ -59,9 +65,20 @@ const validateURL = (url: string): boolean => {
   if (!url) {
     return true;
   }
-  const validateUrlRegex =
-    /(^(https?:\/\/(www\.)?|ftp:\/\/(www\.)?|www\.){1}([0-9A-Za-z-.@:%_+~#=]+)+((\.[a-zA-Z]{2,3})+)(\/(.)*)?(\?(.)*)?)|(http:\/\/localhost:\d+)/;
-  return validateUrlRegex.test(url);
+
+  const protocol = /(?:https?|ftp):\/\//;
+  const optionalWww = /(?:www\.)?/;
+  const hostname = /[0-9A-Za-z\-.@:%_+~#=]+/;
+  const domain = /(?:\.[a-zA-Z]{2,3})+/;
+  const path = /(?:\/[^\s?]*)?/;
+  const query = /(?:\?[^\s]*)?/;
+
+  const standardUrl = new RegExp(
+    `^(?:${protocol.source}${optionalWww.source}|www\\.)${hostname.source}${domain.source}${path.source}${query.source}$`,
+  );
+  const localhost = /http:\/\/localhost:\d+/;
+
+  return standardUrl.test(url) || localhost.test(url);
 };
 
 /**
