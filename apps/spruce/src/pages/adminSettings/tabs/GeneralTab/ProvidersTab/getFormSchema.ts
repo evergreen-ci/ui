d369340bc13323811cc33c2b0cbe3bfdeac9489d
Aaron Hammer <aaronh@athammer.me>
2026-09-17 11:58:36 -0400

    fixing types

--- apps/spruce/src/pages/adminSettings/tabs/GeneralTab/ProvidersTab/getFormSchema.ts
+++ apps/spruce/src/pages/adminSettings/tabs/GeneralTab/ProvidersTab/getFormSchema.ts
@@ -9,64 +9,66 @@ type ResourceTags = {
 export const getFormSchema = ({
   mongodbEnv,
   mongodbOwner,
-}: ResourceTags): ReturnType<GetFormSchema> => ({
-  fields: {},
-  schema: {
-    type: "object" as const,
+}: ResourceTags): ReturnType<GetFormSchema> => {
+  const resourceTagsSchema = {
+    ...aws.schema.resourceTags,
     properties: {
-      providers: {
-        type: "object" as const,
-        title: "",
-        properties: {
-          containerPools: {
-            type: "object" as const,
-            title: "Container Pools",
-            properties: containerPools.schema,
-          },
-          aws: {
-            type: "object" as const,
-            title: "AWS Configuration",
-            properties: {
-              ...aws.schema,
-              resourceTags: {
-                ...aws.schema.resourceTags,
-                properties: {
-                  ...aws.schema.resourceTags.properties,
-                  mongodbEnv: {
-                    ...aws.schema.resourceTags.properties.mongodbEnv,
-                    enum: mongodbEnv
-                      ? aws.schema.resourceTags.properties.mongodbEnv.enum.slice(1)
-                      : aws.schema.resourceTags.properties.mongodbEnv.enum,
-                    enumNames: mongodbEnv
-                      ? aws.schema.resourceTags.properties.mongodbEnv.enumNames.slice(
-                          1,
-                        )
-                      : aws.schema.resourceTags.properties.mongodbEnv.enumNames,
-                  },
-                  mongodbOwner: {
-                    ...aws.schema.resourceTags.properties.mongodbOwner,
-                    ...(mongodbOwner ? { minLength: 1 } : {}),
-                  },
-                },
+      ...aws.schema.resourceTags.properties,
+      mongodbEnv: {
+        ...aws.schema.resourceTags.properties.mongodbEnv,
+        enum: mongodbEnv
+          ? aws.schema.resourceTags.properties.mongodbEnv.enum.slice(1)
+          : aws.schema.resourceTags.properties.mongodbEnv.enum,
+        enumNames: mongodbEnv
+          ? aws.schema.resourceTags.properties.mongodbEnv.enumNames.slice(1)
+          : aws.schema.resourceTags.properties.mongodbEnv.enumNames,
+      },
+      mongodbOwner: {
+        ...aws.schema.resourceTags.properties.mongodbOwner,
+        ...(mongodbOwner ? { minLength: 1 } : {}),
+      },
+    },
+  };
+
+  return {
+    fields: {},
+    schema: {
+      type: "object" as const,
+      properties: {
+        providers: {
+          type: "object" as const,
+          title: "",
+          properties: {
+            containerPools: {
+              type: "object" as const,
+              title: "Container Pools",
+              properties: containerPools.schema,
+            },
+            aws: {
+              type: "object" as const,
+              title: "AWS Configuration",
+              properties: {
+                ...aws.schema,
+                resourceTags: resourceTagsSchema,
               },
             },
-          },
-          docker: {
-            type: "object" as const,
-            title: "Docker",
-            properties: docker.schema,
+            docker: {
+              type: "object" as const,
+              title: "Docker",
+              properties: docker.schema,
+            },
           },
         },
       },
     },
-  },
-  uiSchema: {
-    providers: {
-      containerPools: containerPools.uiSchema,
-      aws: aws.uiSchema,
-      docker: docker.uiSchema,
+    uiSchema: {
+      providers: {
+        containerPools: containerPools.uiSchema,
+        aws: aws.uiSchema,
+        docker: docker.uiSchema,
+      },
     },
-  },
-});
+  };
+};
 
 export const formSchema = getFormSchema({ mongodbEnv: "", mongodbOwner: "" });
