52a19bdc48004ef1b089e2ffb55b3efeaac2682b
Adam Rasheed <adam.rasheed@mongodb.com>
2026-09-17 10:07:28 -0700

    UXE-986: migrate waterfall loading and error states

--- apps/spruce/src/pages/waterfall/WaterfallSkeleton/index.tsx
+++ apps/spruce/src/pages/waterfall/WaterfallSkeleton/index.tsx
@@ -1,20 +1,31 @@
-import {
-  TableSkeleton,
-  TableSkeletonProps,
-} from "@leafygreen-ui/skeleton-loader";
+import { Skeleton, Text } from "@via-ds/components";
 import { VERSION_LIMIT } from "../constants";
+import styles from "./index.module.css";
 
-const WaterfallSkeleton: React.FC<TableSkeletonProps> = ({
+interface WaterfallSkeletonProps {
+  numCols?: number;
+  numRows?: number;
+}
+
+const WaterfallSkeleton: React.FC<WaterfallSkeletonProps> = ({
   numCols = VERSION_LIMIT + 1,
   numRows = 15,
-  ...rest
 }) => (
-  <TableSkeleton
-    data-testid="waterfall-skeleton"
-    numCols={numCols}
-    numRows={numRows}
-    {...rest}
-  />
+  <Skeleton isLoading>
+    <div
+      className={styles.skeleton}
+      data-testid="waterfall-skeleton"
+      style={
+        {
+          "--waterfall-skeleton-columns": numCols,
+        } as React.CSSProperties
+      }
+    >
+      {Array.from({ length: numCols * numRows }, (_, index) => (
+        <Text key={index}>Loading waterfall data</Text>
+      ))}
+    </div>
+  </Skeleton>
 );
 
 export default WaterfallSkeleton;
