0782f02d39761a7430207b18a7a34d097eb638f2
Adam Rasheed <adam.rasheed@mongodb.com>
2026-09-18 12:54:05 -0700

    fix: preserve waterfall loading indicator color

--- apps/spruce/src/pages/waterfall/FetchMoreLoader.tsx
+++ apps/spruce/src/pages/waterfall/FetchMoreLoader.tsx
@@ -3,7 +3,11 @@ import styles from "./FetchMoreLoader.module.css";
 
 export const FetchMoreLoader: React.FC = () => (
   <div className={styles.loader} data-testid="fetch-more-loader">
-    <ProgressCircle aria-label="Fetching more commits" size="4xlarge" />
+    <ProgressCircle
+      aria-label="Fetching more commits"
+      className={styles.progressCircle}
+      size="4xlarge"
+    />
     <Text textStyle="description">Fetching…</Text>
   </div>
 );
