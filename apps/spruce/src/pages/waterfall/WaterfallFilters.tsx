5600952dd19d0588c8a091ea66d0633a473a37f1
Adam Rasheed <adam.rasheed@mongodb.com>
2026-09-18 09:58:39 -0700

    fix: preserve waterfall walkthrough anchor

--- apps/spruce/src/pages/waterfall/WaterfallFilters.tsx
+++ apps/spruce/src/pages/waterfall/WaterfallFilters.tsx
@@ -94,7 +94,6 @@ export const WaterfallFilters: React.FC<WaterfallFiltersProps> = ({
         />
       </ProjectFilterItem>
       <WaterfallMenu
-        key={isWalkthroughMenuStep ? "walkthrough-menu" : "waterfall-menu"}
         isWalkthroughMenuStep={isWalkthroughMenuStep}
         omitInactiveBuilds={omitInactiveBuilds}
         projectIdentifier={projectIdentifier}
